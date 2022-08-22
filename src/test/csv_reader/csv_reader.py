30dbdd07bbaebb6a3fc04cf97c78edc7bce69b84
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-22 17:19:30 +0100

    Skeleton test failing.

---
+++ src/test/csv_reader/csv_reader.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestCsvReader(unittest.TestCase):
+    def test_should_read_csv(self):
+        self.fail("Forced fail on test")
+
+
+if __name__ == '__main__':
+    unittest.main()
