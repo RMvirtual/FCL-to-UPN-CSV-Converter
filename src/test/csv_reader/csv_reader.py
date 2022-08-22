e016b0fe76803bed9e33da41e4d963d4562c6d56
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-22 18:00:59 +0100

    Gets simple_scenario.csv runfile path on test.

--- src/test/csv_reader/csv_reader.py
+++ src/test/csv_reader/csv_reader.py
@@ -1,9 +1,16 @@
 import unittest
+from rules_python.python.runfiles import runfiles
 
 
 class TestCsvReader(unittest.TestCase):
-    def test_should_read_csv(self):
-        self.fail("Forced fail on test")
+    def test_should_get_number_of_rows(self):
+        r = runfiles.Create()
+        location = r.Rlocation(
+            "fcl-to-upn-csv/resources/test_inputs/simple_scenario.csv")
+
+        print("Location:", location)
+
+        self.fail("FAIL ME")
 
 
 if __name__ == '__main__':
