faf7df751fc5ce270918138663d25a2369a64131
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-23 16:24:23 +0100

    Passes test for reading the number of rows from a CSV.

---
+++ src/main/csv/reader.py
@@ -0,0 +1,27 @@
+import csv
+
+
+def read(src_path: str):
+    with open(src_path, newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+        csv_reading = CsvReading()
+
+        for row in reader:
+            csv_reading.add(row)
+
+    return csv_reading
+
+
+class CsvReading:
+    def __init__(self):
+        self._rows = []
+
+    def add(self, row: list[str]):
+        self._rows.append(row)
+
+    def at(self, index: int):
+        return self._rows[index]
+
+    def number_of_rows(self):
+        return len(self._rows)
+
--- src/test/csv_reader/csv_reader.py
+++ src/test/csv_reader/csv_reader.py
@@ -1,4 +1,5 @@
 import unittest
+import src.main.csv.reader as csv_reader
 from rules_python.python.runfiles import runfiles
 
 
@@ -9,8 +10,11 @@ class TestCsvReader(unittest.TestCase):
             "fcl-to-upn-csv/resources/test_inputs/simple_scenario.csv")
 
         csv_file = csv_reader.read(location)
-        self.fail("FAIL ME")
+        rows = csv_file.number_of_rows()
+        correct_no_of_rows = 2
 
+        self.assertEqual(correct_no_of_rows, rows)
+        self.assertNotEqual(0, rows)
 
 if __name__ == '__main__':
     unittest.main()
