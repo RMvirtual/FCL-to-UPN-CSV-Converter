f0a7640127f78cd89e6e88d1830065242db42a43
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-23 17:55:06 +0100

    Compile fail for start of consignment from UPN csv test.

--- src/test/consignment/consignment.py
+++ src/test/consignment/consignment.py
@@ -1,8 +1,20 @@
 import unittest
+from rules_python.python.runfiles import runfiles
 
 
 class TestConsignment(unittest.TestCase):
-    def test_should_read_consignment(self):
+    def setUp(self) -> None:
+        self._initialise_csv_path()
+
+    def _initialise_csv_path(self) -> None:
+        data_files = runfiles.Create()
+
+        self._csv_path = data_files.Rlocation(
+            "fcl-to-upn-csv/resources/test_inputs/simple_scenario.csv")
+
+    def test_should_create_consignment_from_upn_csv(self):
+        consignment = Consignment.fromUpnCsv(self._csv_path)
+
         self.fail("Force fail")
 
 
