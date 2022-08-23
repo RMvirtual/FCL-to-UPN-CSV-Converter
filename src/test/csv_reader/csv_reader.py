37917c013972e89f0e947e6fc768a96a0dd7f97b
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-23 17:07:35 +0100

    Added working (failing) test for reading second row (first row of values).

--- src/test/csv_reader/csv_reader.py
+++ src/test/csv_reader/csv_reader.py
@@ -6,7 +6,7 @@ from rules_python.python.runfiles import runfiles
 class TestCsvReader(unittest.TestCase):
     def setUp(self) -> None:
         self._load_csv_file()
-        self._initialise_correct_headers()
+        self._initialise_correct_values()
 
     def test_should_get_number_of_rows(self):
         rows = self._csv_file.number_of_rows()
@@ -18,6 +18,10 @@ class TestCsvReader(unittest.TestCase):
         headers = self._csv_file.row(0)
         self.assertListEqual(self._correct_headers, headers)
 
+    def test_should_read_second_row(self):
+        second_row = self._csv_file.row(1)
+        self.assertListEqual(self._correct_first_row, second_row)
+
     def _load_csv_file(self) -> None:
         r = runfiles.Create()
 
@@ -25,6 +29,10 @@ class TestCsvReader(unittest.TestCase):
             "fcl-to-upn-csv/resources/test_inputs/simple_scenario.csv")
 
         self._csv_file = csv_reader.read(location)
+
+    def _initialise_correct_values(self) -> None:
+        self._initialise_correct_headers()
+        self._initialise_correct_first_row_values()
 
     def _initialise_correct_headers(self) -> None:
         self._correct_headers = [
@@ -38,6 +46,16 @@ class TestCsvReader(unittest.TestCase):
             "Weight4", "Pkgs.4", "Pkg Type4", "Gds.Desc4",
             "SpecInstr:1:", "SpecInstr:2:", "BI Time", "DelDate",
             "CustRef", "Pallets", "DelService", "Tail Lift Del"
+        ]
+
+    def _initialise_correct_first_row_values(self) -> None:
+        self._correct_first_row = [
+            "Mr  Susan      Cheshire", "10 BRAMBLING RISE", "HEMEL HEMPSTEAD",
+            "", "", "HEMEL HEMPSTEAD", "HP2   6DT", "GR220806951",
+            "(078)4133 2424", "1000", "1", "PALL", "PALLETS N / D",
+            "PROP PAL LTD", "", "", "", "", "", "", "", "", "", "", "", "",
+            "TEL: 07841 332424, TAIL LIFT","", "", "23 - Aug - 22", "",
+            "1", "2", "Yes"
         ]
 
 
