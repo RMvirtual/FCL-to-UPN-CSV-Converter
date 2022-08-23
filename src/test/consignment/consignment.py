754dc78bef4a3fa971e3b256c08bde50d7ee509d
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-23 17:46:15 +0100

    Started work on consignment code. Added failing dummy test.

---
+++ src/test/consignment/consignment.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestConsignment(unittest.TestCase):
+    def test_should_read_consignment(self):
+        self.fail("Force fail")
+
+
+if __name__ == '__main__':
+    unittest.main()
