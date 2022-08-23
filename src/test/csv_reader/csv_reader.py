8202ae9ca9b89a421e2fb87378dddba70f46d9a3
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-23 16:44:22 +0100

    Passes test for getting CSV headers from first row.

--- src/test/csv_reader/csv_reader.py
+++ src/test/csv_reader/csv_reader.py
@@ -6,6 +6,7 @@ from rules_python.python.runfiles import runfiles
 class TestCsvReader(unittest.TestCase):
     def setUp(self) -> None:
         self._load_csv_file()
+        self._initialise_correct_headers()
 
     def test_should_get_number_of_rows(self):
         rows = self._csv_file.number_of_rows()
@@ -13,15 +14,31 @@ class TestCsvReader(unittest.TestCase):
 
         self.assertEqual(correct_no_of_rows, rows)
 
-    def test_should_read_first_line(self):
-        self.fail("Force fail")
+    def test_should_read_first_line_headers(self):
+        headers = self._csv_file.at(0)
+        self.assertListEqual(self._correct_headers, headers)
 
-    def _load_csv_file(self):
+    def _load_csv_file(self) -> None:
         r = runfiles.Create()
+
         location = r.Rlocation(
             "fcl-to-upn-csv/resources/test_inputs/simple_scenario.csv")
 
         self._csv_file = csv_reader.read(location)
+
+    def _initialise_correct_headers(self) -> None:
+        self._correct_headers = [
+            "ContactName", "CompanyName", "StreetName", "Locality",
+            "Town", "County", "Postcode", "ConNo",
+            "ContactPhone",
+            "Weight1", "Pkgs.1", "Pkg Type1", "Gds.Desc1",
+            "Client",
+            "Weight2", "Pkgs.2", "Pkg Type2", "Gds.Desc2",
+            "Weight3", "Pkgs.3", "Pkg Type3", "Gds.Desc3",
+            "Weight4", "Pkgs.4", "Pkg Type4", "Gds.Desc4",
+            "SpecInstr:1:", "SpecInstr:2:", "BI Time", "DelDate",
+            "CustRef", "Pallets", "DelService", "Tail Lift Del"
+        ]
 
 
 if __name__ == '__main__':
