846ba5866f3fbf3b175180e2e6efa569b6110c10
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 09:58:53 +0100

    Added failing test for numeric only consignment number setting.

--- src/test/consignment/consignment.py
+++ src/test/consignment/consignment.py
@@ -6,6 +6,7 @@ from src.main.consignment.consignment import Consignment
 class TestConsignment(unittest.TestCase):
     def setUp(self) -> None:
         self._initialise_csv_path()
+        self._consignment = Consignment()
 
     def _initialise_csv_path(self) -> None:
         data_files = runfiles.Create()
@@ -13,13 +14,17 @@ class TestConsignment(unittest.TestCase):
         self._csv_path = data_files.Rlocation(
             "fcl-to-upn-csv/resources/test_inputs/simple_scenario.csv")
 
-    def test_should_set_consignment_number(self):
-        consignment = Consignment()
-        consignment.reference = "GR220806951"
+    def test_should_set_full_reference(self):
         correct_reference = "GR220806951"
+        self._consignment.reference = "GR220806951"
 
-        self.assertEqual(correct_reference, consignment.reference)
+        self.assertEqual(correct_reference, self._consignment.reference)
 
+    def test_should_set_numeric_only_reference(self):
+        correct_reference = "GR220806951"
+        self._consignment.reference = "220806951"
+
+        self.assertEqual(correct_reference, self._consignment.reference)
 
 if __name__ == '__main__':
     unittest.main()
