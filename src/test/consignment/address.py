6d6d04c810ae65b86eb8f0fb1100ceb64296d81c
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 13:19:44 +0100

    Passes first test for setting address details and validating they are minimally sufficient.

--- src/test/consignment/address.py
+++ src/test/consignment/address.py
@@ -5,6 +5,11 @@ from src.main.consignment.address import Address
 class TestAddress(unittest.TestCase):
     def test_should_complete_address(self):
         address = Address()
+        address.name = "Graylaw Freight Group"
+        address.line_1 = "Gillibrands Road"
+        address.town = "Skelmersdale"
+        address.post_code = "WN8 9TA"
+        address.country = "GB"
 
         self.assertTrue(address.is_valid())
 
