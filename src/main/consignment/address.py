984bbdfb1bb75779e0852ae23788b536aa0b0560
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 12:31:50 +0100

    Start of address class and failing test case.

---
+++ src/main/consignment/address.py
@@ -0,0 +1,6 @@
+
+
+class Address:
+    def __init__(self):
+        pass
+
---
+++ src/test/consignment/address.py
@@ -0,0 +1,11 @@
+import unittest
+from src.main.consignment.address import Address
+
+
+class TestAddress(unittest.TestCase):
+    def test_should_set_address(self):
+        self.fail("Force fail")
+
+
+if __name__ == '__main__':
+    unittest.main()
