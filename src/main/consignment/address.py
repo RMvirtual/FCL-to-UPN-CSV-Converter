5c1e00d51b5f89c9578937436ecb7e70926b9d10
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 12:38:21 +0100

    Work on address class and validity check as basis for test.

--- src/main/consignment/address.py
+++ src/main/consignment/address.py
@@ -2,5 +2,20 @@
 
 class Address:
     def __init__(self):
-        pass
+        self._name = ""
+        self._line_1 = ""
+        self._line_2 = ""
+        self._line_3 = ""
+        self._town = ""
+        self._post_code = ""
+        self._country = "GB"
+        self._telephone_number = ""
 
+    def is_valid(self):
+        return (
+            self._name != ""
+            and self._line_1 != ""
+            and self._town != ""
+            and self._post_code != ""
+            and self._country != ""
+        )
--- src/test/consignment/address.py
+++ src/test/consignment/address.py
@@ -3,8 +3,10 @@ from src.main.consignment.address import Address
 
 
 class TestAddress(unittest.TestCase):
-    def test_should_set_address(self):
-        self.fail("Force fail")
+    def test_should_complete_address(self):
+        address = Address()
+
+        self.assertTrue(address.is_valid())
 
 
 if __name__ == '__main__':
