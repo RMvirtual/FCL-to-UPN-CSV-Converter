a4a13ab6b195f638a7bb84db793a834901614a7e
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 17:11:15 +0100

    Uploading work on cargo entry. Needs further thought, with possible JSON data file.

---
+++ src/main/consignment/cargo/entry.py
@@ -0,0 +1,11 @@
+
+
+class CargoEntry:
+    def __init__(self):
+        self._pallet_type = None
+        self._total_weight = 0
+
+
+
+
+
--- src/test/consignment/cargo.py
+++ src/test/consignment/cargo.py
@@ -1,8 +1,12 @@
 import unittest
+from src.main.consignment.cargo.entry import CargoEntry
 
 
-class TestCargo(unittest.TestCase):
-    def test_should_add_pallet(self):
+class TestCargoEntry(unittest.TestCase):
+    def test_should_create_cargo_entry(self):
+        cargo_line = CargoEntry()
+        cargo_line.pallet_type = None
+
         self.fail("Force fail on cargo module")
 
 
