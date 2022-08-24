6185becd2bf834e7a9cd66faf76feae4114b83e9
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 16:34:02 +0100

    Started cargo module. Failing dummy test added.

---
+++ src/test/consignment/cargo.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestCargo(unittest.TestCase):
+    def test_should_add_pallet(self):
+        self.fail("Force fail on cargo module")
+
+
+if __name__ == '__main__':
+    unittest.main()
