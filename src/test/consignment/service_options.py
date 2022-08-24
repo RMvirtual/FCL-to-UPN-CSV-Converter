3cdd4ad75df359d040afb6b705489911597a5238
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-24 16:08:58 +0100

    Passes default checks.

--- src/test/consignment/service_options.py
+++ src/test/consignment/service_options.py
@@ -3,8 +3,18 @@ from src.main.consignment.service_options import ServiceOptions
 
 
 class TestServiceOptions(unittest.TestCase):
-    def test_service_options(self):
-        self.fail("Dummy fail on service option test.")
+    def should_create_default_service_options(self):
+        service_options = ServiceOptions()
+
+        checks = [
+            service_options.main_service.is_next_day(),
+            service_options.premium_service.is_not_required(),
+            service_options.additional_service.is_not_required(),
+            service_options.tail_lift.is_not_required()
+        ]
+
+        for check in checks:
+            self.assertTrue(check)
 
 
 if __name__ == '__main__':
