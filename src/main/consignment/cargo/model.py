a214b725f8a63fa2e8791b1875fb18cb6d39cf53
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-25 15:39:56 +0100

    Passes first part of test for combining cargo entries into one line.

--- src/main/consignment/cargo/model.py
+++ src/main/consignment/cargo/model.py
@@ -6,6 +6,13 @@ class Cargo:
         self._entry_lines: list[CargoEntry] = []
 
     def add(self, cargo_entry: CargoEntry) -> None:
+        for entry in self._entry_lines:
+            if entry.pallet_type.__class__ is cargo_entry.pallet_type.__class__:
+                entry.total_weight += cargo_entry.total_weight
+                entry.number_of_pallets += cargo_entry.number_of_pallets
+
+                return
+
         self._entry_lines.append(cargo_entry)
 
     def __iter__(self):
