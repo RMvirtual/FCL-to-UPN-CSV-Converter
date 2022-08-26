a5adb7952404cb0e9fd5a77ab234914d29663739
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-26 14:42:11 +0100

    Refactored Cargo tests further.

--- src/test/consignment/cargo.py
+++ src/test/consignment/cargo.py
@@ -1,4 +1,5 @@
 import unittest
+from typing import Type
 from src.main.consignment.cargo.entry import CargoEntry
 from src.main.consignment.cargo.types import *
 from src.main.consignment.cargo.model import Cargo
@@ -9,28 +10,19 @@ class TestCargo(unittest.TestCase):
         self._cargo = Cargo()
 
     def test_should_add_a_single_cargo_entry(self):
-        entry = self._cargo_entry(FullPallet(), (1, 1000))
-        self._cargo.add(entry)
-
-        self.assertIsInstance(self._cargo[0].package_type, FullPallet)
-        self.assertEqual(1, self._cargo[0].quantity)
-        self.assertEqual(1000, self._cargo[0].weight_kgs)
+        self._create_and_add_cargo_entry(FullPallet(), (1, 1000))
+        self._compare_cargo_entry(0, FullPallet, (1, 1000))
 
     def test_should_combine_two_cargo_entries(self):
-        entry_1 = self._cargo_entry(HalfPallet(), (1, 400))
-        entry_2 = self._cargo_entry(HalfPallet(), (2, 1000))
-
-        self._cargo.add(entry_1)
-        self._cargo.add(entry_2)
+        self._create_and_add_cargo_entry(HalfPallet(), (1, 400))
+        self._create_and_add_cargo_entry(HalfPallet(), (2, 1000))
 
         correct_no_of_entries = 1
-        correct_total_weight = 1400
-        correct_total_pallets = 3
+        correct_type = HalfPallet
+        correct_qty_and__weight = (3, 1400)
 
         self.assertEqual(correct_no_of_entries, len(self._cargo))
-        self.assertEqual(correct_total_pallets, self._cargo[0].quantity)
-        self.assertEqual(correct_total_weight, self._cargo[0].weight_kgs)
-        self.assertIsInstance(self._cargo[0].package_type, HalfPallet)
+        self._compare_cargo_entry(0, correct_type, correct_qty_and__weight)
 
     def test_should_reject_combining_two_cargo_entries(self):
         entry_1 = self._cargo_entry(HalfPallet(), (1, 400))
@@ -40,20 +32,14 @@ class TestCargo(unittest.TestCase):
             entry_1 += entry_2
 
     def test_should_add_two_different_pallet_types(self):
-        entry_1 = self._cargo_entry(HalfPallet(), (1, 400))
-        entry_2 = self._cargo_entry(FullPallet(), (3, 1000))
-        self._cargo.add(entry_1)
-        self._cargo.add(entry_2)
+        self._create_and_add_cargo_entry(HalfPallet(), (1, 400))
+        self._create_and_add_cargo_entry(FullPallet(), (3, 1000))
 
-        self.assertEqual(2, len(self._cargo))
+        correct_no_of_entries = 2
 
-        self.assertEqual(1, self._cargo[0].quantity)
-        self.assertEqual(400, self._cargo[0].weight_kgs)
-        self.assertIsInstance(self._cargo[0].package_type, HalfPallet)
-
-        self.assertEqual(3, self._cargo[1].quantity)
-        self.assertEqual(1000, self._cargo[1].weight_kgs)
-        self.assertIsInstance(self._cargo[1].package_type, FullPallet)
+        self.assertEqual(correct_no_of_entries, len(self._cargo))
+        self._compare_cargo_entry(0, HalfPallet, (1, 400))
+        self._compare_cargo_entry(1, FullPallet, (3, 1000))
 
     def test_should_not_exceed_max_weight_when_modifying_weight(self):
         entry = self._cargo_entry(FullPallet(), (3, 3000))
@@ -68,17 +54,21 @@ class TestCargo(unittest.TestCase):
             entry.quantity = 2
 
     def test_should_modify_qty_and_weight(self):
-        entry = self._cargo_entry(FullPallet(), (1, 1000))
-        entry.quantity_and_weight = (3, 3000)
+        self._create_and_add_cargo_entry(FullPallet(), (1, 1000))
+        self._cargo[0].quantity_and_weight = (3, 3000)
 
-        self.assertEqual(3, entry.quantity)
-        self.assertEqual(3000, entry.weight_kgs)
+        self._compare_cargo_entry(0, FullPallet, (3, 3000))
 
     def test_should_not_exceed_max_weight_when_modifying_qty_and_weight(self):
         entry = self._cargo_entry(FullPallet(), (3, 3000))
 
         with self.assertRaises(ValueError):
             entry.quantity_and_weight = (4, 8000)
+
+    def _create_and_add_cargo_entry(
+            self, pkg_type: Pallet, qty_and_weight: tuple[int, float]) -> None:
+        entry = self._cargo_entry(pkg_type, qty_and_weight)
+        self._cargo.add(entry)
 
     @staticmethod
     def _cargo_entry(
@@ -87,6 +77,14 @@ class TestCargo(unittest.TestCase):
         entry.quantity_and_weight = qty_and_weight
 
         return entry
+
+    def _compare_cargo_entry(
+            self, index: int, pkg_type: Type[Pallet],
+            quantity_and_weight: tuple[int, float]) -> None:
+
+        self.assertIsInstance(self._cargo[index].package_type, pkg_type)
+        self.assertEqual(quantity_and_weight[0], self._cargo[index].quantity)
+        self.assertEqual(quantity_and_weight[1], self._cargo[index].weight_kgs)
 
 
 if __name__ == '__main__':
