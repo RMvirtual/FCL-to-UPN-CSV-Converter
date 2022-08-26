76cf867013843f4c5376fc0976e50e2f791d32df
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-26 15:59:51 +0100

    Started work on FCL CSV parser.

---
+++ src/test/consignment/database.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestDatabase(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_should_(self):
+        self.fail("Force fail")
---
+++ src/test/csv/fcl_parser.py
@@ -0,0 +1,14 @@
+import unittest
+from rules_python.python.runfiles import runfiles
+
+
+class TestFclCsvParser(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_should_read_simple_scenario_into_a_consignment(self):
+        self.fail("Dummy fail on FCL CSV Parser")
+
+
+if __name__ == '__main__':
+    unittest.main()
