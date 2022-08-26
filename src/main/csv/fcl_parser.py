78230c1adbafc68e5748a096c19cc01cf3c71b7f
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-26 16:24:47 +0100

    Split tests into smaller tests.

--- src/main/csv/fcl_parser.py
+++ src/main/csv/fcl_parser.py
@@ -2,6 +2,6 @@ from src.main.csv.reader import read as read_csv
 from src.main.consignment.consignment import Consignment
 
 
-def read(csv_path: str) -> list[Consignment]:
+def read(csv_path: str, ignore_headers: bool = False) -> list[Consignment]:
     return []
 
--- src/test/csv/fcl.py
+++ src/test/csv/fcl.py
@@ -7,10 +7,38 @@ class TestFclCsvParser(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_should_read_simple_scenario_into_a_consignment(self):
-        consignments = fcl_csv_parser.read(self._simple_scenario_csv)
+    def test_should_read_a_quantity_of_one_consignment(self):
+        consignments = fcl_csv_parser.read(
+            csv_path=self._simple_scenario_csv,
+            ignore_headers=True
+        )
 
         self.assertEqual(1, len(consignments))
+
+    def test_should_read_a_consignment_reference(self):
+        consignments = fcl_csv_parser.read(
+            csv_path=self._simple_scenario_csv,
+            ignore_headers=True
+        )
+
+        reference = consignments[0].reference
+        self.assertEqual(reference, "GR220806951")
+
+    def test_should_read_a_consignment_address(self):
+        consignments = fcl_csv_parser.read(
+            csv_path=self._simple_scenario_csv,
+            ignore_headers=True
+        )
+
+        address = consignments[0].address
+        self.assertEqual(address.name, "10 BRAMBLING RISE")
+        self.assertEqual(address.line_1, "HEMEL HEMPSTEAD")
+        self.assertEqual(address.line_2, "")
+        self.assertEqual(address.line_3, "")
+        self.assertEqual(address.town, "HEMEL HEMPSTEAD")
+        self.assertEqual(address.post_code, "HP2 6DT")
+        self.assertEqual(address.country, "GB")
+        self.assertEqual(address.telephone_number, "(078) 4133 2424")
 
     @property
     def _simple_scenario_csv(self) -> str:
