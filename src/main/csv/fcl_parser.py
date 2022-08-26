2f722d22f17d64b9235cb3ff353a92cfb8ebef9a
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-26 17:40:14 +0100

    Added full list of column heading enum constants for the FCL csv.

--- src/main/csv/fcl_parser.py
+++ src/main/csv/fcl_parser.py
@@ -15,7 +15,40 @@ class FclCsvFormat:
     """Interprets a list of strings as a UPNEDIIMP FCL CSV export."""
 
     class Column(IntEnum):
+        CONTACT_NAME = 0
+        COMPANY_NAME = 1
+        LINE_1 = 2
+        LINE_2 = 3
+        LINE_3 = 4
+        TOWN = 5
+        POST_CODE = 6
         REFERENCE = 7
+        TELEPHONE_NO = 8
+        LINE_1_WEIGHT = 9
+        LINE_1_QUANTITY = 10
+        LINE_1_PACKAGE_TYPE = 11
+        LINE_1_DESCRIPTION = 12
+        PRINCIPAL_CLIENT = 13
+        LINE_2_WEIGHT = 14
+        LINE_2_QUANTITY = 15
+        LINE_2_PACKAGE_TYPE = 16
+        LINE_2_DESCRIPTION = 17
+        LINE_3_WEIGHT = 18
+        LINE_3_QUANTITY = 19
+        LINE_3_PACKAGE_TYPE = 20
+        LINE_3_DESCRIPTION = 21
+        LINE_4_WEIGHT = 22
+        LINE_4_QUANTITY = 23
+        LINE_4_PACKAGE_TYPE = 24
+        LINE_4_DESCRIPTION = 25
+        DELIVERY_INSTRUCTION_1 = 26
+        DELIVERY_INSTRUCTION_2 = 27
+        BOOKING_TIME = 28
+        DELIVERY_DATE = 29
+        SHIPPER_REFERENCE = 30
+        TOTAL_PALLETS = 31
+        PRIORITY_CODE = 32
+        TAIL_LIFT_REQUIRED = 33
 
     def __init__(self):
         self._consignments: dict[str, Consignment] = {}
