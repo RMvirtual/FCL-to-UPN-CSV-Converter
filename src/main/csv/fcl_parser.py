7ff1527ac3837aa1ab32b6f699b79db9be02358f
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-08-26 16:40:13 +0100

    Passes test for reading one FCL consignment (although not a great test).

--- src/main/csv/fcl_parser.py
+++ src/main/csv/fcl_parser.py
@@ -3,5 +3,7 @@ from src.main.consignment.consignment import Consignment
 
 
 def read(csv_path: str, ignore_headers: bool = False) -> list[Consignment]:
-    return []
+    csv_rows = read_csv(src_path=csv_path, ignore_headers=ignore_headers)
+
+    return csv_rows
 
--- src/main/csv/reader.py
+++ src/main/csv/reader.py
@@ -1,13 +1,16 @@
 import csv
 
 
-def read(src_path: str) -> list[list[str]]:
+def read(src_path: str, ignore_headers: bool = False) -> list[list[str]]:
     with open(src_path, newline='') as csv_file:
         reader = csv.reader(csv_file, delimiter=',', quotechar='"')
         csv_readings = []
 
-        for row in reader:
-            csv_readings.append(row)
+        starting_index = 1 if ignore_headers else 0
 
-    return csv_readings
+        for row in reader:
+            reading = row
+            csv_readings.append(reading)
+
+    return csv_readings[starting_index:]
 
