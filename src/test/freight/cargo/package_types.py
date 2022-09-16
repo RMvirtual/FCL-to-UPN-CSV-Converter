f6ab6b92fae900e10b3f99eaff70ef18680d10dd
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-09-16 17:26:15 +0100

    Failing test for value error raising on incorrect package type being assigned.

--- src/test/freight/cargo/package_types.py
+++ src/test/freight/cargo/package_types.py
@@ -18,6 +18,12 @@ class TestPackageTypesJsonReader(unittest.TestCase):
         self.assertEqual("double", package_type.oversize_option)
         self.assertEqual(2, package_type.oversize_multiplier)
 
+    def test_should_error_when_setting_invalid_oversize_option(self):
+        package_type = load_package_type("quarter")
+
+        with self.assertRaises(ValueError):
+            package_type.oversize_option = "quadruple"
+
     @property
     def _test_json_file(self) -> str:
         return load_path("resources/test_inputs/base_packages.json")
