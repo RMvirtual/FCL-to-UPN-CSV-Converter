77a52909a21e9c9d7475c31d208427acbd8bdd9a
RMvirtual <rmvirtual@AOL.com>
2022-09-18 18:56:35 +0100

    Refactored UPNEDIIMP csv imports.

--- src/main/forward_office/dashboard/export/upn_edi_imp_csv.py
+++ src/main/forward_office/dashboard/export/upn_edi_imp_csv.py
@@ -1,15 +1,14 @@
-import src.main.forward_office.dashboard.fields.upn_edi_imp as \
-    upn_edi_fcl_fields
-import src.main.forward_office.dashboard.export.file_formats.csv as csv_reader
+from src.main.forward_office.dashboard.fields.upn_edi_imp \
+    import upn_edi_imp_format
+
+from src.main.forward_office.dashboard.export.file_formats import csv
 
 
 def import_upn_edi_imp_csv_fcl_export(csv_path: str):
-    fields = upn_edi_fcl_fields.upn_edi_imp_format()
+    fields = upn_edi_imp_format()
 
-    stuff = csv_reader.read(
+    return csv.read(
         csv_path=csv_path,
         field_indexes=fields,
         ignore_headers=True
     )
-
-    return stuff
