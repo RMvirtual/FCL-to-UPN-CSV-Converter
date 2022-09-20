c3ff702ab1f87cf83aac5af251566e1aeb1790af
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-09-20 17:47:19 +0100

    Passes address parser test.

--- src/test/forward_office/dashboard/export/parser/address.py
+++ src/test/forward_office/dashboard/export/parser/address.py
@@ -1,13 +1,12 @@
 import unittest
 
-from src.main.file_system.runfiles import load_path
 from src.main.forward_office.dashboard.export.parser.address \
     import AddressParser
 
 
 class TestAddressParser(unittest.TestCase):
     def setUp(self) -> None:
-        self._fields = {
+        self._field_indexes = {
             "contact_name": 0,
             "company_name": 1,
             "address_line_1": 2,
@@ -18,8 +17,21 @@ class TestAddressParser(unittest.TestCase):
             "telephone_no": 8
         }
 
+        self._values_to_parse = [
+            "Mr Susan Cheshire",
+            "10 BRAMBLING RISE",
+            "HEMEL HEMPSTEAD",
+            "",
+            "",
+            "HEMEL HEMPSTEAD",
+            "HP2 6DT",
+            "...",
+            "(078) 4133 2424"
+        ]
+
     def test_should_read_a_consignment_address(self):
-        address = list(self._consignments.values())[0].address
+        address_parser = AddressParser(self._field_indexes)
+        address = address_parser.parse(self._values_to_parse)
         self.assertEqual(address.name, "10 BRAMBLING RISE")
         self.assertEqual(address.line_1, "HEMEL HEMPSTEAD")
         self.assertEqual(address.line_2, "")
@@ -29,10 +41,6 @@ class TestAddressParser(unittest.TestCase):
         self.assertEqual(address.country, "GB")
         self.assertEqual(address.contact_name, "Mr Susan Cheshire")
         self.assertEqual(address.telephone_number, "(078) 4133 2424")
-
-    @property
-    def _simple_scenario_csv(self) -> str:
-        return load_path("resources/test_inputs/simple_scenario.csv")
 
 
 if __name__ == '__main__':
