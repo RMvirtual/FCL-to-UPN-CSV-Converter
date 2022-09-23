a33609b47f0b0a28d8512776c06e4110e4b7d9ef
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-09-23 17:53:08 +0100

    Start of cargo parser processing FCL goods description lines 1 - 4.

--- src/main/forward_office/dashboard/export/parser/cargo.py
+++ src/main/forward_office/dashboard/export/parser/cargo.py
@@ -8,22 +8,29 @@ class CargoParser:
     def __init__(self, field_indexes: dict[str, int]):
         self._fields = field_indexes
         self._cargo = Cargo()
+        self._mappings = FclCargoTypeMap()
 
     def parse(self, values: list[str]) -> Cargo:
         self._cargo.clear()
-
-        mappings = FclCargoTypeMap()
-
-        if values[self._fields["line_1_package_type"]] != "":
-            package_type = getattr(
-                mappings, values[self._fields["line_1_package_type"]])
-
-            new_entry = CargoEntry(package_type)
-            new_entry.quantity = int(values[self._fields["line_1_quantity"]])
-            new_entry.weight_kgs = float(values[self._fields["line_1_weight"]])
-            self._cargo.add(new_entry)
+        self._parse_cargo_line("line_1", values)
 
         return self._cargo
+
+    def _parse_cargo_line(self, line_number: str, values):
+        short_code = values[self._fields[line_number + "_package_type"]]
+
+        if short_code:
+            package_type = getattr(self._mappings, short_code)
+
+            new_entry = CargoEntry(package_type)
+
+            new_entry.quantity = int(
+                values[self._fields[line_number + "_quantity"]])
+
+            new_entry.weight_kgs = float(
+                values[self._fields[line_number + "_weight"]])
+
+            self._cargo.add(new_entry)
 
     def _extract_value(self, csv_row: list[str], field: str) -> str:
         field_column_index = self._fields[field]
