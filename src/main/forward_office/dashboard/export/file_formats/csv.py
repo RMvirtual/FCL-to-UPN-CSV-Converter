38cb19bd0f0b650fe792562e10483fe1ab531a39
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-09-23 16:19:23 +0100

    Removed more hardcoded formats.

--- src/main/forward_office/dashboard/export/file_formats/csv.py
+++ src/main/forward_office/dashboard/export/file_formats/csv.py
@@ -1,22 +1,24 @@
+from dataclasses import dataclass
+
 from src.main.csv.reader import read as read_csv
 from src.main.freight.consignment.consignment import Consignment
 from src.main.forward_office.dashboard.export.parser.consignment \
     import ConsignmentParser
 
 
+@dataclass
 class ReadParameters:
-    def __init__(self):
-        self.csv_path: str = ""
-        self.field_indexes: dict[str, int] = {}
-        self.ignore_headers: bool = False
+    dashboard_format: dict[str, int]
+    csv_path: str = ""
+    ignore_headers: bool = False
 
 
 def read(parameters: ReadParameters) -> dict[str, Consignment]:
     csv_rows = read_csv(
         src_path=parameters.csv_path,
-        ignore_headers=parameters.field_indexes
+        ignore_headers=parameters.dashboard_format
     )
 
-    parser = ConsignmentParser(parameters.field_indexes)
+    parser = ConsignmentParser(parameters.dashboard_format)
 
     return parser.parse(csv_rows)
