b067e5f3d03388337edc5f8f7dad39b440ad4a84
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-09-23 15:55:36 +0100

    Removed older hardcoded UPNEDIIMP reader.

--- src/main/forward_office/dashboard/export/upn_edi_imp_csv.py
+++ src/main/forward_office/dashboard/export/upn_edi_imp_csv.py
@@ -1,4 +1,4 @@
-from src.main.forward_office.dashboard.fields import upn_edi_imp
+from src.main.forward_office.dashboard import formats
 from src.main.forward_office.dashboard.export.file_formats import csv
 
 
@@ -7,6 +7,6 @@ def import_upn_edi_imp_csv_fcl_export(csv_path: str):
 
     read_parameters.csv_path = csv_path
     read_parameters.ignore_headers = True
-    read_parameters.field_indexes = upn_edi_imp.format()
+    read_parameters.field_indexes = formats.DashboardFormats().UPNEDIIMP
 
     return csv.read(read_parameters)
