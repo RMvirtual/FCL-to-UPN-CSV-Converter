5fccb608f9f3ee44ed4b655edf1a69bf470dd877
RMvirtual <rmvirtual@AOL.com>
2022-09-24 10:39:23 +0100

    Refactored format construction.

--- src/main/forward_office/dashboard/format/all.py
+++ src/main/forward_office/dashboard/format/all.py
@@ -4,23 +4,28 @@ import json
 from src.main.file_system import runfiles
 from src.main.file_system.dashboard_format_files import DashboardFormatFiles
 
-format_files = DashboardFormatFiles()
-formats = []
 
-for format_field in dataclasses.fields(format_files):
-    full_format_path = runfiles.load_path(
-        getattr(format_files, format_field.name))
+def _get_formats():
+    format_files = DashboardFormatFiles()
+    formats = []
 
-    with open(full_format_path) as json_stream:
-        format_contents = json.load(json_stream)
+    for format_field in dataclasses.fields(format_files):
+        full_format_path = runfiles.load_path(
+            getattr(format_files, format_field.name))
 
-    formats.append([
-        format_field.name,
-        dict[str, int],
-        dataclasses.field(default_factory=lambda: format_contents)
-    ])
+        with open(full_format_path) as json_stream:
+            format_contents = json.load(json_stream)
+
+        formats.append([
+            format_field.name,
+            dict[str, int],
+            dataclasses.field(default_factory=lambda: format_contents)
+        ])
+
+    return formats
+
 
 DashboardFormats = dataclasses.make_dataclass(
     cls_name="DashboardFormats",
-    fields=formats
+    fields=_get_formats()
 )
