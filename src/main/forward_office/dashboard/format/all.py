6c6a14fd566fb130b63adbdf964731e974dd8dba
RMvirtual <rmvirtual@AOL.com>
2022-09-24 10:59:05 +0100

    Refactored format loader utility class.

--- src/main/forward_office/dashboard/format/all.py
+++ src/main/forward_office/dashboard/format/all.py
@@ -5,39 +5,42 @@ from src.main.file_system import runfiles
 from src.main.file_system.dashboard_format_files import DashboardFormatFiles
 
 
-def _formats():
-    format_files = DashboardFormatFiles()
-    formats = []
+class FormatLoader:
+    def __init__(self):
+        self._initialise_formats()
 
-    for file_name in dataclasses.fields(format_files):
-        full_format_path = _get_file_path(file_name, format_files)
-        format_contents = _load_contents_from_json(full_format_path)
+    def all(self):
+        return self._formats
 
-        formats.append(_create_dashboard_format(file_name, format_contents))
+    def _initialise_formats(self):
+        format_files = DashboardFormatFiles()
+        self._formats = []
 
-    return formats
+        for file_entry in dataclasses.fields(format_files):
+            full_format_path = self._file_path(file_entry, format_files)
+            format_contents = self._load_contents_from_json(full_format_path)
 
+            self._formats.append(
+                self._create_dashboard_format(file_entry, format_contents))
 
-def _create_dashboard_format(file_name, contents):
-    return [
-        file_name.name,
-        dict[str, int],
-        dataclasses.field(default_factory=lambda: contents)
-    ]
+    def _create_dashboard_format(self, file_name, contents):
+        return [
+            file_name.name,
+            dict[str, int],
+            dataclasses.field(default_factory=lambda: contents)
+        ]
 
+    def _file_path(self, file_entry: any, format_files: DashboardFormatFiles):
+        return runfiles.load_path(getattr(format_files, file_entry.name))
 
-def _get_file_path(file_name: any, format_files: DashboardFormatFiles):
-    return runfiles.load_path(getattr(format_files, file_name.name))
+    def _load_contents_from_json(self, file_path: str):
+        with open(file_path) as json_stream:
+            format_contents = json.load(json_stream)
 
-
-def _load_contents_from_json(file_path: str):
-    with open(file_path) as json_stream:
-        format_contents = json.load(json_stream)
-
-    return format_contents
+        return format_contents
 
 
 DashboardFormats = dataclasses.make_dataclass(
     cls_name="DashboardFormats",
-    fields=_formats()
+    fields=FormatLoader().all()
 )
