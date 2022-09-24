8c72aba49d75ea25c57a5445216d1e51fff6d432
RMvirtual <rmvirtual@AOL.com>
2022-09-24 10:44:15 +0100

    Refactored runfile getter for format_files.

--- src/main/forward_office/dashboard/format/all.py
+++ src/main/forward_office/dashboard/format/all.py
@@ -5,19 +5,18 @@ from src.main.file_system import runfiles
 from src.main.file_system.dashboard_format_files import DashboardFormatFiles
 
 
-def _get_formats():
+def _formats():
     format_files = DashboardFormatFiles()
     formats = []
 
-    for format_field in dataclasses.fields(format_files):
-        full_format_path = runfiles.load_path(
-            getattr(format_files, format_field.name))
+    for file_name in dataclasses.fields(format_files):
+        full_format_path = _get_file_path(file_name, format_files)
 
         with open(full_format_path) as json_stream:
             format_contents = json.load(json_stream)
 
         formats.append([
-            format_field.name,
+            file_name.name,
             dict[str, int],
             dataclasses.field(default_factory=lambda: format_contents)
         ])
@@ -25,7 +24,11 @@ def _get_formats():
     return formats
 
 
+def _get_file_path(file_name: any, format_files: DashboardFormatFiles):
+    return runfiles.load_path(getattr(format_files, file_name.name))
+
+
 DashboardFormats = dataclasses.make_dataclass(
     cls_name="DashboardFormats",
-    fields=_get_formats()
+    fields=_formats()
 )
