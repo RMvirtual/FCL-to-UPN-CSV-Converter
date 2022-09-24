f2fe35ca47a324a67c068870c4bbb3e3a0f1228a
RMvirtual <rmvirtual@AOL.com>
2022-09-24 12:13:16 +0100

    Used list comprehension on dashboard format files.

--- src/main/file_system/dashboard_format_files.py
+++ src/main/file_system/dashboard_format_files.py
@@ -6,20 +6,20 @@ from src.main.file_system import system_files, runfiles
 
 def _format_files():
     contents = _json_contents()
-    fields = []
 
-    for item in contents:
-        fields.append([item, str, contents[item]])
-
-    return fields
+    return [
+        [item, str, contents[item]] for item in contents]
 
 
 def _json_contents():
-    dashboard_formats_file = system_files.load_path("FCL_DASHBOARD_FORMATS")
-    full_path = runfiles.load_path(dashboard_formats_file)
-
-    with open(full_path) as json_file:
+    with open(_file_path()) as json_file:
         return json.load(json_file)
+
+
+def _file_path():
+    dashboard_formats_file = system_files.load_path("FCL_DASHBOARD_FORMATS")
+
+    return runfiles.load_path(dashboard_formats_file)
 
 
 DashboardFormatFiles = dataclasses.make_dataclass(
