1f1e999eec9410e2ec3aa18d37db415c284493b9
RMvirtual <rmvirtual@AOL.com>
2022-09-24 12:07:52 +0100

    Refactored dashboard format files json loading.

--- src/main/file_system/dashboard_format_files.py
+++ src/main/file_system/dashboard_format_files.py
@@ -5,18 +5,21 @@ from src.main.file_system import system_files, runfiles
 
 
 def _format_files():
-    dashboard_formats_file = system_files.load_path("FCL_DASHBOARD_FORMATS")
-    full_path = runfiles.load_path(dashboard_formats_file)
-
-    with open(full_path) as json_file:
-        contents = json.load(json_file)
-
+    contents = _json_contents()
     fields = []
 
     for item in contents:
         fields.append([item, str, contents[item]])
 
     return fields
+
+
+def _json_contents():
+    dashboard_formats_file = system_files.load_path("FCL_DASHBOARD_FORMATS")
+    full_path = runfiles.load_path(dashboard_formats_file)
+
+    with open(full_path) as json_file:
+        return json.load(json_file)
 
 
 DashboardFormatFiles = dataclasses.make_dataclass(
