cdba7299f5196a6898dc094c01862273646faf2a
RMvirtual <rmvirtual@AOL.com>
2022-09-24 10:49:49 +0100

    Extracted more dashboard format construction functions.

--- src/main/forward_office/dashboard/format/all.py
+++ src/main/forward_office/dashboard/format/all.py
@@ -11,21 +11,30 @@ def _formats():
 
     for file_name in dataclasses.fields(format_files):
         full_format_path = _get_file_path(file_name, format_files)
+        format_contents = _load_contents_from_json(full_format_path)
 
-        with open(full_format_path) as json_stream:
-            format_contents = json.load(json_stream)
-
-        formats.append([
-            file_name.name,
-            dict[str, int],
-            dataclasses.field(default_factory=lambda: format_contents)
-        ])
+        formats.append(_create_dashboard_format(file_name, format_contents))
 
     return formats
 
 
+def _create_dashboard_format(file_name, contents):
+    return [
+        file_name.name,
+        dict[str, int],
+        dataclasses.field(default_factory=lambda: contents)
+    ]
+
+
 def _get_file_path(file_name: any, format_files: DashboardFormatFiles):
     return runfiles.load_path(getattr(format_files, file_name.name))
+
+
+def _load_contents_from_json(file_path: str):
+    with open(file_path) as json_stream:
+        format_contents = json.load(json_stream)
+
+    return format_contents
 
 
 DashboardFormats = dataclasses.make_dataclass(
