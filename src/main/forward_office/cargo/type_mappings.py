db80d949e15e45d06527fffc9120795567ec5eba
RMvirtual <rmvirtual@AOL.com>
2022-09-24 11:23:10 +0100

    Started refactoring cargo type mappings.

--- src/main/forward_office/cargo/type_mappings.py
+++ src/main/forward_office/cargo/type_mappings.py
@@ -5,35 +5,47 @@ import copy
 from src.main.file_system import runfiles, system_files
 from src.main.freight.cargo.types import load_package_type, PackageType
 
-cargo_type_mappings_file = system_files.load_path("FCL_CARGO_TYPE_MAPPINGS")
-full_path = runfiles.load_path(cargo_type_mappings_file)
 
-with open(full_path) as json_file:
-    contents = json.load(json_file)
+class Loader:
+    def __init__(self):
+        self._load()
 
-short_codes_to_package_types = {}
+    def _load(self):
+        cargo_type_mappings_file = system_files.load_path(
+            "FCL_CARGO_TYPE_MAPPINGS")
 
-for package_type in contents:
-    short_code = package_type["short_code"]
-    mapping_info = package_type["maps_to"]
+        full_path = runfiles.load_path(cargo_type_mappings_file)
 
-    the_type = load_package_type(mapping_info["name"])
-    the_type.oversize_option = mapping_info["oversize_option"]
+        with open(full_path) as json_file:
+            contents = json.load(json_file)
 
-    short_codes_to_package_types[short_code] = the_type
+        short_codes_to_package_types = {}
 
-field_interpretation = []
+        for package_type in contents:
+            short_code = package_type["short_code"]
+            mapping_info = package_type["maps_to"]
 
-for short_code in short_codes_to_package_types:
-    package_type = short_codes_to_package_types[short_code]
+            the_type = load_package_type(mapping_info["name"])
+            the_type.oversize_option = mapping_info["oversize_option"]
 
-    field_interpretation.append([
-        short_code,
-        PackageType,
-        dataclasses.field(default=package_type)
-    ])
+            short_codes_to_package_types[short_code] = the_type
+
+        self._field_interpretation = []
+
+        for short_code in short_codes_to_package_types:
+            package_type = short_codes_to_package_types[short_code]
+
+            self._field_interpretation.append([
+                short_code,
+                PackageType,
+                dataclasses.field(default=package_type)
+            ])
+
+    def all(self):
+        return copy.copy(self._field_interpretation)
+
 
 FclCargoTypeMap = dataclasses.make_dataclass(
     cls_name="FclCargoTypeMap",
-    fields=field_interpretation
+    fields=Loader().all()
 )
--- src/main/forward_office/dashboard/format/all.py
+++ src/main/forward_office/dashboard/format/all.py
@@ -8,31 +8,34 @@ from src.main.file_system.dashboard_format_files import DashboardFormatFiles
 
 class FormatLoader:
     def __init__(self):
+        self._initialise()
         self._load_formats()
 
-    def _load_formats(self):
+    def _initialise(self):
         self._format_files = DashboardFormatFiles()
         self._formats = []
 
-        for file_entry in dataclasses.fields(self._format_files):
-            self.add_format_entry(file_entry)
+    def _load_formats(self):
+        for format_file in dataclasses.fields(self._format_files):
+            self._add(format_file)
 
-    def add_format_entry(self, format_entry):
-        full_format_path = self._file_path(format_entry)
-        format_contents = self._json_contents(full_format_path)
-        self._add_format(format_entry, format_contents)
+    def _add(self, format_file):
+        file_path = self._file_path(format_file)
+        format_contents = self._json_contents(file_path)
 
-    def _add_format(self, file_name, contents):
+        self._add_format(format_file.name, format_contents)
+
+    def _file_path(self, format_file: dataclasses.dataclass):
+        return runfiles.load_path(getattr(self._format_files, format_file.name))
+
+    def _add_format(self, format_name: str, format_contents):
         new_format = [
-            file_name.name,
+            format_name,
             dict[str, int],
-            dataclasses.field(default_factory=lambda: contents)
+            dataclasses.field(default_factory=lambda: format_contents)
         ]
 
         self._formats.append(new_format)
-
-    def _file_path(self, file_entry: any):
-        return runfiles.load_path(getattr(self._format_files, file_entry.name))
 
     @staticmethod
     def _json_contents(file_path: str):
