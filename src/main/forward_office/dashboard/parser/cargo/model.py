033d95cb1039c0829099b25dab91465427d16cfb
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-03 15:24:15 +0100

    Refactored list of function pointers for handling cargo parse requests.

--- src/main/forward_office/dashboard/parser/cargo/model.py
+++ src/main/forward_office/dashboard/parser/cargo/model.py
@@ -6,6 +6,7 @@ from src.main.forward_office.dashboard.parser.cargo import validation
 from src.main.forward_office.dashboard.parser.cargo.validation \
     import CargoParseRequest, CargoParseErrors
 
+
 class CargoParser:
     def __init__(self, field_indexes: dict[str, int]):
         self._fields = field_indexes
@@ -31,14 +32,16 @@ class CargoParser:
         self._process(errors) if errors else self._process(request)
 
     def _process(self, request: CargoParseRequest or CargoParseErrors) -> None:
-        if type(request) is validation.CargoParseRequest:
-            self._process_cargo_entry(request)
+        request_type_handlers = {
+            CargoParseRequest: self._process_cargo_entry,
+            CargoParseErrors: self._process_error
+        }
 
-        elif type(request) is validation.CargoParseErrors:
-            self._process_error(request)
+        if type(request) in request_type_handlers:
+            request_type_handlers[type(request)](request)
 
         else:
-            raise TypeError("Invalid type supplied.")
+            raise TypeError("Invalid type.")
 
     @staticmethod
     def _process_error(errors):
@@ -46,22 +49,25 @@ class CargoParser:
             raise validation.CargoParseException(
                 message="Cargo parse errors", errors=errors)
 
-    def _create_parse_requests(self, values):
-        result = []
+    def _create_parse_requests(self, values: list[str]):
+        return [
+            self._request_by_line(line_number, values)
+            for line_number in range(1, 5)
+        ]
 
-        for line_number in ["line_1", "line_2", "line_3", "line_4"]:
-            result.append(self._create_parse_request(line_number, values))
-
-        return result
-
-    def _create_parse_request(self, line_number, values):
+    def _request_by_line(self, line_number: int, values: list[str]):
         result = validation.CargoParseRequest()
 
-        result.short_code = values[self._fields[line_number + "_package_type"]]
-        result.quantity = values[self._fields[line_number + "_quantity"]]
-        result.weight = values[self._fields[line_number + "_weight"]]
+        result.short_code = values[self._field(line_number, "package_type")]
+        result.quantity = values[self._field(line_number, "quantity")]
+        result.weight = values[self._field(line_number, "weight")]
 
         return result
+
+    def _field(self, line_number: int, name: str) -> int:
+        full_field = "line_" + str(line_number) + "_" + name
+
+        return self._fields[full_field]
 
     def _process_cargo_entry(self, request: validation.CargoParseRequest):
         package_type = getattr(self._mappings, request.short_code)
