c158d212f56d26a14d65acffb7900a8aca9568ba
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-04 11:06:58 +0100

    Start of consignment parser.

---
+++ src/test/forward_office/dashboard/parser/consignment.py
@@ -0,0 +1,63 @@
+import unittest
+
+
+class TestConsignmentParser(unittest.TestCase):
+    def setUp(self) -> None:
+        self._field_indexes = {
+            "contact_name": 0,
+            "company_name":  1,
+            "address_line_1": 2,
+            "address_line_2": 3,
+            "address_line_3": 4,
+            "town": 5,
+            "post_code": 6,
+            "reference": 7,
+            "telephone_no": 8,
+            "line_1_weight": 9,
+            "line_1_quantity": 10,
+            "line_1_package_type": 11,
+            "line_1_description": 12,
+            "principal_client": 13,
+            "line_2_weight": 14,
+            "line_2_quantity": 15,
+            "line_2_package_type": 16,
+            "line_2_description": 17,
+            "line_3_weight": 18,
+            "line_3_quantity": 19,
+            "line_3_package_type": 20,
+            "line_3_description": 21,
+            "line_4_weight": 22,
+            "line_4_quantity": 23,
+            "line_4_package_type": 24,
+            "line_4_description": 25,
+            "delivery_instruction_1": 26,
+            "delivery_instruction_2": 27,
+            "booking_time": 28,
+            "delivery_date": 29,
+            "shipper_reference": 30,
+            "total_pallets": 31,
+            "priority_code": 32,
+            "tail_lift_required": 33
+        }
+
+        self._simple_example = [
+            "Mr Susan Cheshire", "10 BRAMBLING RISE",
+            "HEMEL HEMPSTEAD", "", "", "HEMEL HEMPSTEAD", "HP2 6DT",
+            "GR220806951", "(078)413 32424",
+            "1000", "1", "PALL", "PALLETS N/D",
+            "PROP PAL LTD",
+            "", "", "",  "",
+            "", "", "", "",
+            "", "", "", "",
+            "TEL: 07841 332424, TAIL LIFT", "",
+            "", "23-Aug-22",
+            "", "1",
+            "2", "Yes"
+        ]
+
+    def test_should_parse_consignment(self):
+        self.fail("Dummy fail on consignment parser.")
+
+
+if __name__ == '__main__':
+    unittest.main()
