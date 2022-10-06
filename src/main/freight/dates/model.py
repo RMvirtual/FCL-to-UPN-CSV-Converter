9ebbd3bce5a666b50d2d4a1b65ca8dd589388fe8
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-06 14:45:18 +0100

    Passes date logical operator tests.

--- src/main/freight/dates/model.py
+++ src/main/freight/dates/model.py
@@ -1,11 +1,75 @@
+from __future__ import annotations
 import datetime
+
+
+class Date:
+    def __init__(self, date: str):
+        date = date.replace("/", "")
+        day = int(date[0:2])
+        month = int(date[2:4])
+        year = int(date[4:8])
+
+        self._date = datetime.date(day=day, month=month, year=year)
+
+    @property
+    def day(self) -> int:
+        return self._date.day
+
+    @property
+    def month(self) -> int:
+        return self._date.month
+
+    @property
+    def year(self) -> int:
+        return self._date.year
+
+    def __eq__(self, other: Date) -> bool:
+        return (
+            self.day == other.day and self.month == other.month
+            and self.year == other.year
+        )
+
+    def __lt__(self, other: Date) -> bool:
+        if self.year < other.year:
+            return True
+
+        elif self.year == other.year:
+            if self.month < other.month:
+                return True
+
+            elif self.month == other.month:
+                if self.day < other.day:
+                    return True
+
+        return False
+
+    def __le__(self, other: Date) -> bool:
+        return self < other or self == other
+
+    def __ge__(self, other: Date) -> bool:
+        return self > other or self == other
+
+    def __gt__(self, other: Date) -> bool:
+        if self.year > other.year:
+            return True
+
+        elif self.year == other.year:
+            if self.month > other.month:
+                return True
+
+            elif self.month == other.month:
+                if self.day > other.day:
+                    return True
+
+        return False
+
 
 
 class ShipmentDates:
     def __init__(self):
-        self._collection_date: datetime.date or None = None
-        self._delivery_date: datetime.date or None = None
-        self._delivery_time: datetime.time or None = None
+        self._collection_date: Date or None = None
+        self._delivery_date: Date or None = None
+        self._delivery_time: Date or None = None
 
     @property
     def delivery_date(self) -> datetime.date:
--- src/test/freight/dates/dates.py
+++ src/test/freight/dates/dates.py
@@ -1,13 +1,20 @@
 import unittest
-from src.main.freight.dates.model import ShipmentDates
+from src.main.freight.dates.model import ShipmentDates, Date
 
 
 class TestShipmentDates(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_dates_logically_operate_relative_to_time_periods(self):
-        self.fail("DUMMY FAIL FOR DATES LOGICAL OPERATORS")
+    def test_dates_can_be_compared(self):
+        date_1 = Date("06/10/2022")
+        date_2 = Date("07/10/2022")
+
+        self.assertTrue(date_1 < date_2)
+        self.assertTrue(date_1 <= date_2)
+        self.assertFalse(date_1 == date_2)
+        self.assertFalse(date_1 > date_2)
+        self.assertFalse(date_1 >= date_2)
 
 
 if __name__ == '__main__':
