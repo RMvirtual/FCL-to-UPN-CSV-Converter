d1f150b0f719a928e85808d82fe2bd40999d9577
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-06 12:16:33 +0100

    Multiple trigger instructions for tail lift advisories added to test.

--- src/test/freight/consignment/validation.py
+++ src/test/freight/consignment/validation.py
@@ -1,13 +1,17 @@
 import unittest
 
 from src.main.freight.consignment.validation \
-    import ConsignmentValidationStrategy
+    import ConsignmentValidationStrategy, ConsignmentErrors
 
 from src.main.freight.consignment.model import Consignment
 from src.main.freight.cargo import types
 
 
 class TestConsignmentValidation(unittest.TestCase):
+    def setUp(self):
+        self._validation = ConsignmentValidationStrategy()
+        self._load_basic_consignment()
+
     def _load_basic_consignment(self):
         self._consignment = Consignment()
         self._consignment.address.name = "Ryan Matfen"
@@ -25,21 +29,27 @@ class TestConsignmentValidation(unittest.TestCase):
             types.load_package_type("full"))
 
     def test_should_highlight_tail_lift_advisory(self):
-        self._load_basic_consignment()
-        self._consignment.delivery_instructions.append("Tail Lift")
+        danger_instructions = [
+            "Tail lift req", "TL required", "needs t/l",
+            "Give meeee a T/L", "customer requires t-lift",
+            "send tail-lift"
+        ]
 
-        validation = ConsignmentValidationStrategy()
-        errors = validation.validate_tail_lift_error(self._consignment)
-
-        self.assertTrue(errors.tail_lift_advisory)
+        for instruction in danger_instructions:
+            errors = self._tail_lift_errors_from_instruction(instruction)
+            self.assertTrue(errors.tail_lift_advisory, msg=instruction)
 
     def test_should_not_highlight_tail_lift_advisory(self):
-        self._load_basic_consignment()
-
-        validation = ConsignmentValidationStrategy()
-        errors = validation.validate_tail_lift_error(self._consignment)
+        errors = self._validation.validate_tail_lift_error(self._consignment)
 
         self.assertFalse(errors.tail_lift_advisory)
+
+    def _tail_lift_errors_from_instruction(
+            self, instruction: str) -> ConsignmentErrors:
+        self._consignment.delivery_instructions.clear()
+        self._consignment.delivery_instructions.append(instruction)
+
+        return self._validation.validate_tail_lift_error(self._consignment)
 
 
 if __name__ == '__main__':
