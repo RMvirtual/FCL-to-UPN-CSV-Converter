9dd88b01cde3595f4dab35087feae88d2c4643fc
RMvirtual <rmvirtual@AOL.com>
2022-10-08 12:28:46 +0100

    Cleaner implementation for splitting dates by separator.

--- src/main/freight/dates/transforms.py
+++ src/main/freight/dates/transforms.py
@@ -9,10 +9,14 @@ def parse(date: str) -> tuple[int, int, int]:
         return int(date[0:2]), int(date[2:4]), (2000 + int(date[4:6]))
 
     elif re.fullmatch(r"\d{2}[./\\-]\d{2}[./\\-]\d{2}", date):  # dd\mm\yy
-        return int(date[0:2]), int(date[3:5]), (2000 + int(date[6:8]))
+        day, month, year = re.split(r"[./\\-]", date)
+
+        return int(day), int(month), (2000 + int(year))
 
     elif re.fullmatch(r"\d{2}[./\\-]\d{2}[./\\-]\d{4}", date):  # dd\mm\yyyy
-        return int(date[0:2]), int(date[3:5]), (int(date[6:10]))
+        day, month, year = re.split(r"[./\\-]", date)
+
+        return int(day), int(month), int(year)
 
     else:
         return None
