166425e92210eb49a386b8dc7d0ca0157df8f507
RMvirtual <rmvirtual@AOL.com>
2022-10-08 12:22:32 +0100

    Factored dates without separators recognition.

--- src/main/freight/dates/transforms.py
+++ src/main/freight/dates/transforms.py
@@ -2,10 +2,10 @@ import re
 
 
 def parse(date: str) -> tuple[int, int, int]:
-    if re.fullmatch(r"\d{2}\d{2}\d{4}", date):  # ddmmyyyy
+    if re.fullmatch(r"\d{8}", date):  # ddmmyyyy
         return int(date[0:2]), int(date[2:4]), int(date[4:8])
 
-    elif re.fullmatch(r"\d{2}\d{2}\d{2}", date):  # ddmmyy
+    elif re.fullmatch(r"\d{6}", date):  # ddmmyy
         return int(date[0:2]), int(date[2:4]), (2000 + int(date[4:6]))
 
     elif re.fullmatch(r"\d{2}[./\\-]\d{2}[./\\-]\d{2}", date):  # dd\mm\yy
