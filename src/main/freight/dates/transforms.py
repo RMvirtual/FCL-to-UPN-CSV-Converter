94b38e60436c5e3a4343bde57ee2ef073cd94cf8
RMvirtual <rmvirtual@AOL.com>
2022-10-09 09:57:00 +0100

    Failing tests added for dd-mmm-yyyy formats.

--- src/main/freight/dates/transforms.py
+++ src/main/freight/dates/transforms.py
@@ -9,12 +9,12 @@ def parse(date: str) -> tuple[int, int, int]:
         return int(date[0:2]), int(date[2:4]), int("20" + date[4:6])
 
     elif re.fullmatch(r"\d{2}[./\\-]\d{2}[./\\-]\d{2}", date):  # dd\mm\yy
-        day, month, year = _split_by_separators(date)
+        day, month, year = _split_by_separator_characters(date)
 
         return int(day), int(month), int("20" + year)
 
     elif re.fullmatch(r"\d{2}[./\\-]\d{2}[./\\-]\d{4}", date):  # dd\mm\yyyy
-        day, month, year = _split_by_separators(date)
+        day, month, year = _split_by_separator_characters(date)
 
         return int(day), int(month), int(year)
 
@@ -22,5 +22,5 @@ def parse(date: str) -> tuple[int, int, int]:
         return None
 
 
-def _split_by_separators(value: str) -> tuple[str]:
+def _split_by_separator_characters(value: str) -> tuple[str]:
     return re.split(r"[./\\-]", value)
--- src/test/freight/dates/transforms.py
+++ src/test/freight/dates/transforms.py
@@ -42,6 +42,18 @@ class TestDateTransforms(unittest.TestCase):
                     "Failed format:", format)
             )
 
+    def test_should_transform_dd_mmm_yyyy_(self):
+        date_formats = [
+            "23-Oct-2022", "23-Oct-22"]
+
+        for format in date_formats:
+            day, month, year = transforms.parse(format)
+
+            self.assertTupleEqual(
+                (23, 10, 2022), (day, month, year), msg=(
+                    "Failed format:", format)
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
