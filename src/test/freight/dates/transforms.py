d56335747a54fd2048712b2701002c1eb3359eea
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-10 14:35:14 +0100

    Minimised testing lines for date transforms.

--- src/test/freight/dates/transforms.py
+++ src/test/freight/dates/transforms.py
@@ -7,70 +7,48 @@ class TestDateTransforms(unittest.TestCase):
         pass
 
     def test_should_transform_ddmmyyyy_without_separators(self):
-        day, month, year = transforms.parse("23102022")
-
-        self.assertTupleEqual((23, 10, 2022), (day, month, year))
+        self._assert_date_transform_produces((23, 10, 2022), "23102022")
 
     def test_should_transform_ddmmyy_without_separators(self):
-        day, month, year = transforms.parse("231022")
-
-        self.assertTupleEqual((23, 10, 2022), (day, month, year))
+        self._assert_date_transform_produces((23, 10, 2022), "231022")
 
     def test_should_transform_ddmmyy_with_separators(self):
-        dates = ["23\\10\\22", "23-10-22", "23/10/22", "23.10.22"]
-
-        for date in dates:
-            day, month, year = transforms.parse(date)
-
-            self.assertTupleEqual(
-                (23, 10, 2022), (day, month, year),
-                msg=("Failed format:", date)
-            )
+        self._assert_date_transforms_produce(
+            (23, 10, 2022), ["23\\10\\22", "23-10-22", "23/10/22", "23.10.22"])
 
     def test_should_transform_ddmmyyyy_with_separators(self):
-        dates = ["23\\10\\2022", "23-10-2022", "23/10/2022", "23.10.2022"]
-
-        for date in dates:
-            day, month, year = transforms.parse(date)
-
-            self.assertTupleEqual(
-                (23, 10, 2022), (day, month, year),
-                msg=("Failed format:", date)
-            )
+        self._assert_date_transforms_produce(
+            (23, 10, 2022),
+            ["23\\10\\2022", "23-10-2022", "23/10/2022", "23.10.2022"]
+        )
 
     def test_should_transform_dd_mmm_yyyy_(self):
-        dates = ["23-Oct-2022", "23-Oct-22"]
-
-        for date in dates:
-            day, month, year = transforms.parse(date)
-
-            self.assertTupleEqual(
-                (23, 10, 2022), (day, month, year),
-                msg=("Failed format:", date)
-            )
+        self._assert_date_transforms_produce(
+            (23, 10, 2022), ["23-Oct-2022", "23-Oct-22"])
 
     def test_should_transform_dd_mmmmm_yyyy_(self):
-        dates = ["23-April-2022", "23-April-22"]
-
-        for date in dates:
-            day, month, year = transforms.parse(date)
-
-            self.assertTupleEqual(
-                (23, 4, 2022), (day, month, year), msg=("Failed format:", date)
-            )
+        self._assert_date_transforms_produce(
+            (23, 4, 2022), ["23-April-2022", "23-April-22"])
 
     def test_should_transform_date_with_full_month_name(self):
-        dates = ["23-September-2022", "23/September/22", " 23 September 2022"]
+        self._assert_date_transforms_produce(
+            (23, 9, 2022),
+            ["23-September-2022", "23/September/22", " 23 September 2022"]
+        )
 
+    def _assert_date_transforms_produce(
+            self, correct_dd_mm_yyyy: tuple[int, int, int],
+            dates: list[str]
+    ) -> None:
         for date in dates:
-            self._assert_date_transform_produces((23, 9, 2022), date)
+            self._assert_date_transform_produces(correct_dd_mm_yyyy, date)
 
     def _assert_date_transform_produces(
             self, correct_dd_mm_yyyy: tuple[int, int, int], date: str) -> None:
-        self.assertTupleEqual(
-            correct_dd_mm_yyyy, transforms.parse(date),
-            msg="Failed date:" + date
-        )
+        fail_message = "Failed date: " + date
+        dd_mm_yyyy = transforms.parse(date)
+
+        self.assertTupleEqual(correct_dd_mm_yyyy, dd_mm_yyyy, msg=fail_message)
 
 
 if __name__ == '__main__':
