cbabd7f4c13a3179fcaec8081ddf96dcc7aaec46
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-11 16:16:12 +0100

    Start of upn library.

---
+++ src/test/upn/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestUpn(unittest.TestCase):
+    def test_should_fail_while_set_up_in_progress(self):
+        self.fail("DUMMY FAIL ON UPN")
+
+
+if __name__ == '__main__':
+    unittest.main()
