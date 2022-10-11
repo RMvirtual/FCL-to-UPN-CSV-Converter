ffc655c9e58168829e24b31ada0b158f78d8ad33
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-11 14:01:22 +0100

    Reduced repetition in dateformatrecognition tests.

--- src/test/freight/dates/transforms/format_recognition.py
+++ src/test/freight/dates/transforms/format_recognition.py
@@ -4,23 +4,64 @@ from src.main.freight.dates.format_recognition import DateFormatRecognition
 
 class TestDateFormatRecognition(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self._valid_dates = []
+        self._invalid_dates = []
 
     def test_should_recognise_numeric_only_format(self):
-        numeric_dates = ["231022", "23102022"]
+        self._valid_dates = ["231022", "23102022"]
 
-        non_numeric_dates = [
+        self._invalid_dates = [
             "23/10/22", "23/10/2022", "23 September 2022", "23.10.22",
             "23.10.2022", "23 10 2022", "23 10 22"
         ]
 
-        for date in numeric_dates:
-            recognition = DateFormatRecognition(date)
-            self.assertTrue(recognition.is_numeric())
+        self._predicate_against_values(DateFormatRecognition.is_numeric)
 
-        for date in non_numeric_dates:
-            recognition = DateFormatRecognition(date)
-            self.assertFalse(recognition.is_numeric())
+    def test_should_recognise_alphabetic_format(self):
+        self._valid_dates = ["23 September 2022"]
+
+        self._invalid_dates = [
+            "231022", "23102022", "23/10/22", "23/10/2022",
+            "23.10.22", "23.10.2022", "23 10 2022", "23 10 22"
+        ]
+
+        self._predicate_against_values(DateFormatRecognition.is_alphanumeric)
+
+    def test_should_recognise_separated_formats(self):
+        self._valid_dates = [
+            "23/10/22", "23/10/2022", "23.10.22", "23.10.2022"]
+
+        self._invalid_dates = [
+            "231022", "23102022", "23 10 2022", "23 10 22",
+            "23 September 2022"
+        ]
+
+        self._predicate_against_values(DateFormatRecognition.has_separators)
+
+    def test_should_recognise_whitespace_formats(self):
+        self._valid_dates = [
+            "23 10 2022", "23 10 22", "23 September 2022"]
+
+        self._invalid_dates = [
+            "23/10/22", "23/10/2022", "23.10.22", "23.10.2022",
+            "231022", "23102022"
+        ]
+
+        self._predicate_against_values(DateFormatRecognition.has_whitespace)
+
+    def _predicate_against_values(self, callback):
+        self._predicate_valid_values_are_true(callback)
+        self._predicate_invalid_values_are_false(callback)
+
+    def _predicate_valid_values_are_true(self, predicate_callback) -> None:
+        for date in self._valid_dates:
+            self.assertTrue(
+                predicate_callback(DateFormatRecognition(date)), msg=date)
+
+    def _predicate_invalid_values_are_false(self, predicate_callback) -> None:
+        for date in self._invalid_dates:
+            self.assertFalse(
+                predicate_callback(DateFormatRecognition(date)), msg=date)
 
 
 if __name__ == '__main__':
