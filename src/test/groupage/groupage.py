102e33bb33cafd5a382919bd71d6d5086a7dc45f
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-20 09:35:09 +0100

    Started groupage library. Failing test added.

---
+++ src/test/groupage/groupage.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestGroupage(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_should_create_groupage(self):
+        self.fail("DUMMY FAIL ON GROUPAGE")
+
+
+if __name__ == '__main__':
+    unittest.main()
