764edfc4fdcf89c582e7f6b3632494bfd7b60052
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-21 15:46:46 +0100

    Shorted system file load path function.

--- src/main/file_system/file_contents/system_files.py
+++ src/main/file_system/file_contents/system_files.py
@@ -3,14 +3,9 @@ from src.main.file_system.file_readers import runfiles
 
 def load_path(file_name: str):
     sys_files = runfiles.load_json_file("resources/file_system.json")
-    result = None
 
-    for file in sys_files:
-        if file == file_name:
-            result = sys_files[file]
-            break
+    if file_name not in sys_files:
+        raise ValueError(
+            "No system file found with name of " + file_name + ".")
 
-    if result is None:
-        raise ValueError("No file found matching", file_name)
-
-    return result
+    return sys_files[file_name]
