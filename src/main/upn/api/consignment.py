02bb2c85eddccfce646bcb43e2f3e1b1e8dc5f40
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-10-31 09:53:25 +0000

    Made the api environment and client member variables.

--- src/main/upn/api/consignment.py
+++ src/main/upn/api/consignment.py
@@ -4,38 +4,34 @@ from src.main.file_system.file_contents import api_files
 
 class ConsignmentApiCall:
     def __init__(self):
-        pass
+        self._environment = api_files.upn_api()
+        self._client = zeep.Client(self._environment["url"])
 
-    @staticmethod
-    def get_network_input(date: str) -> list[dict]:
+    def get_network_input(self, date: str) -> list[dict]:
         """Returns a list of inbound consignments, collections
         requested and non-network input for a particular day.
         :param date: A string in yyyy-mm-dd format.
         """
-        upn_environment = api_files.upn_api()
-        client = zeep.Client(upn_environment["url"])
-
         despatch_date = "2022-10-18T09:00:00"
         test_job = "GR221005418"
         barcode = "W213391123C"
 
-        return client.service.GetNetworkInput(
+        return self._client.service.GetNetworkInput(
             Depot=75,
             JobDate=date,
-            Username=upn_environment["username"],
-            Password=upn_environment["password"]
+            Username=self._environment["username"],
+            Password=self._environment["password"]
         )
 
-    @staticmethod
-    def get_post_code_restrictions(post_code: str) -> list[dict]:
+    def get_post_code_restrictions(self, post_code: str) -> list[dict]:
         """Returns a list of restriction information for a particular
         postcode.
-        """
-        upn_environment = api_files.upn_api()
-        client = zeep.Client(upn_environment["url"])
 
-        return client.service.GetPostcodeRestrictions(
+        :param post_code: A string representing the postcode area code
+        (e.g. "AB10", "L24" or "EC2N").
+        """
+        return self._client.service.GetPostcodeRestrictions(
             Postcode=post_code,
-            Username=upn_environment["username"],
-            Password=upn_environment["password"]
+            Username=self._environment["username"],
+            Password=self._environment["password"]
         )
