68ccdaabad22218a05522aea6beb3e4b9eebbc7c
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-07 15:46:53 +0000

    Failing test for UPN network consignment structure populating fields.

--- src/main/file_system/file_contents/upn.py
+++ src/main/file_system/file_contents/upn.py
@@ -11,39 +11,19 @@ class MappingValues:
 
 def network_consignment_contents():
     contents = runfiles.load_json_file(
-        "resources/upn/network_consignment.json")
+        "resources/upn/api_structures/network_consignment.json")
 
-    for item in contents:
-        entry = dataclasses.make_dataclass(
-            cls_name=item,
-            fields=[]
-        )
+    fields = []
+
+    for key, values in list(contents.items()):
+        field = [key, MappingValues, MappingValues()]
+        print(field)
+        fields.append(field)
+
+    return fields
+
 
 NetworkConsignment = dataclasses.make_dataclass(
     cls_name="NetworkConsignment",
     fields=network_consignment_contents()
-)
-
-def cargo_type_mappings():
-    relative_path = system_files.load_path("FCL_CARGO_TYPE_MAPPINGS")
-
-    return runfiles.load_json_file(relative_path)
-
-
-def service_code_mappings():
-    relative_path = system_files.load_path("FCL_SERVICE_CODE_MAPPINGS")
-
-    return runfiles.load_json_file(relative_path)
-
-
-def _format_files():
-    relative_path = system_files.load_path("FCL_DASHBOARD_FORMATS")
-    contents = runfiles.load_json_file(relative_path)
-
-    return [[item, str, contents[item]] for item in contents]
-
-
-DashboardFormatFiles = dataclasses.make_dataclass(
-    cls_name="DashboardFormatFiles",
-    fields=_format_files()
 )
--- src/test/file_system/file_contents/upn.py
+++ src/test/file_system/file_contents/upn.py
@@ -1,12 +1,24 @@
 import unittest
+from src.main.file_system.file_contents import upn
 
 
 class TestNetworkConsignmentStruct(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_should_get_network_consignment_structure(self):
-        self.fail("DUMMY FAIL ON NETWORK CONSIGNMENT STRUCTURE")
+    def test_should_create_fields(self):
+        structure = upn.NetworkConsignment()
+
+        self.assertTrue(hasattr(structure, "consignment_no"))
+        self.assertTrue(hasattr(structure.consignment_no, "type"))
+        self.assertTrue(hasattr(structure.consignment_no, "mapping"))
+        self.assertTrue(hasattr(structure.consignment_no, "values"))
+
+    def test_should_populate_fields(self):
+        structure = upn.NetworkConsignment().consignment_no
+
+        self.assertEqual(structure.type, "string")
+        self.assertEqual(structure.mapping, "ConNo")
 
 
 if __name__ == '__main__':
