d4a3fd7afdbe4f9e538609923a2040220a73e5d0
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-07 15:57:10 +0000

    Passes test for populating field with strict values.

--- src/test/file_system/file_contents/upn.py
+++ src/test/file_system/file_contents/upn.py
@@ -14,11 +14,19 @@ class TestNetworkConsignmentStruct(unittest.TestCase):
         self.assertTrue(hasattr(structure.consignment_no, "mapping"))
         self.assertTrue(hasattr(structure.consignment_no, "values"))
 
-    def test_should_populate_fields(self):
+    def test_should_populate_field_without_strict_values(self):
         structure = upn.NetworkConsignment().consignment_no
 
         self.assertEqual(structure.type, "string")
         self.assertEqual(structure.mapping, "ConNo")
+        self.assertListEqual(structure.values, [])
+
+    def test_should_populate_field_with_strict_values(self):
+        structure = upn.NetworkConsignment().additional_service
+
+        self.assertEqual(structure.type, "string")
+        self.assertEqual(structure.mapping, "ExtraService")
+        self.assertListEqual(structure.values, ["", "BI", "Bkd", "OOH"])
 
 
 if __name__ == '__main__':
