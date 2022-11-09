86bb492d9f5ceb05d9a40fa28301b9906fac9ca5
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-09 14:58:58 +0000

    Set up of the parser on the network pallet parser test.

---
+++ src/main/upn/api/parser/network_pallet.py
@@ -0,0 +1,11 @@
+from src.main.upn.api.structures.network_pallet.structure import NetworkPallet
+
+from src.main.upn.api.structures.network_pallet.mapping \
+    import NetworkPalletStructure
+
+
+class NetworkPalletParser:
+    def __init__(self):
+        self._structure = NetworkPalletStructure()
+
+
--- src/test/upn/api/structures/parser/network_pallet.py
+++ src/test/upn/api/structures/parser/network_pallet.py
@@ -1,9 +1,10 @@
 import unittest
+from src.main.upn.api.parser.network_pallet import NetworkPalletParser
 
 
 class TestNetworkPalletParser(unittest.TestCase):
     def setUp(self):
-        pass
+        self._parser = NetworkPalletParser()
 
     def test_should_parse_network_pallet(self):
         self.fail("DUMMY NETWORK PALLET FAIL")
