054e25386f0070305bcf0d9f3ec547ee346e0ac5
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-10 17:09:08 +0000

    Set up network pallet test to debug type setting fail.

---
+++ src/test/upn/api/data_structures/network_pallet/structure.py
@@ -0,0 +1,19 @@
+import unittest
+from src.main.upn.api.data_structures.network_pallet.structure \
+    import NetworkPallet
+
+
+class TestNetworkPallet(unittest.TestCase):
+    def setUp(self):
+        self._pallet = NetworkPallet()
+
+    def test_should_set_type(self):
+        self._pallet.type = "FULL"
+        self.assertEqual("FULL", self._pallet.type)
+
+        self._pallet.type = "MICRO"
+        self.assertEqual("MICRO", self._pallet.type)
+
+
+if __name__ == '__main__':
+    unittest.main()
