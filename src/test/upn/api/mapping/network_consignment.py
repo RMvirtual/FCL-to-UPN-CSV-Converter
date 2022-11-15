42933c79b6131a28c333feb8ff264fd21ddd8465
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-15 15:59:17 +0000

    Dummy test for mapping a network consignment into an in house consignment. Reorganised test scripts.

---
+++ src/test/upn/api/mapping/network_consignment.py
@@ -0,0 +1,12 @@
+import unittest
+from src.main.upn.api.mapping.network_consignment \
+    import NetworkConsignmentConversion
+
+
+class TestNetworkConsignmentMapping(unittest.TestCase):
+    def test_should_map_network_consignment_to_house_consignment(self):
+        self.fail("DUMMY FAIL")
+
+
+if __name__ == '__main__':
+    unittest.main()
