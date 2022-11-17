dc72f1021a2a86447857973a6e0a63419a5dcbf7
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-17 16:45:31 +0000

    Shorter oversize option deserialisation name.

--- src/main/freight/cargo/packages/oversize/factory.py
+++ src/main/freight/cargo/packages/oversize/factory.py
@@ -7,7 +7,7 @@ def all_options() -> dict[str, list[OversizeOption]]:
 
     for entry in oversize_options.contents():
         base_type_name = entry["base_type"]
-        results[base_type_name] = _deserialise_oversize_options(entry)
+        results[base_type_name] = _deserialise_options(entry)
 
     return results
 
@@ -24,8 +24,7 @@ def options_by_base_type(base_type_name: str) -> list[OversizeOption]:
     return result
 
 
-def _deserialise_oversize_options(
-        values: dict[str, any]) -> list[OversizeOption]:
+def _deserialise_options(values: dict[str, any]) -> list[OversizeOption]:
     return [
         OversizeOption(name=option["name"], multiplier=option["multiplier"])
         for option in values["options"]
