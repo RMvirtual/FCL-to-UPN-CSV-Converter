d5a77d84c81ff6e786585db2f7e7196e8926918d
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-17 10:30:55 +0000

    Set up dummy failing test for mapping a network pallet into a house pallet.

---
+++ src/test/upn/api/mapping/network_pallet/network_pallet.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestNetworkPalletMapping(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_should_convert_network_pallet_into_house_pallet(self) -> None:
+        self.fail("DUMMY FAIL")
+
+
+if __name__ == '__main__':
+    unittest.main()
