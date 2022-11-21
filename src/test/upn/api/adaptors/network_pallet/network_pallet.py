afa0322b97ee2808dd02777ea05b2d9b1614b2c0
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-21 12:27:46 +0000

    More work on network pallet adaptor tests.

--- src/test/upn/api/adaptors/network_pallet/network_pallet.py
+++ src/test/upn/api/adaptors/network_pallet/network_pallet.py
@@ -5,30 +5,38 @@ from src.test.upn.api.adaptors.network_pallet import setup
 
 class TestNetworkPalletMapping(unittest.TestCase):
     def setUp(self):
-        self._normal_full_pallet = NetworkPalletAdaptor(
+        self._full_normal_pallet = NetworkPalletAdaptor(
             setup.full_normal_pallet())
 
-        self._double_half_pallet_adaptor = NetworkPalletAdaptor(
+        self._double_half_pallet = NetworkPalletAdaptor(
             setup.double_half_pallet())
 
     def test_should_adapt_base_type(self) -> None:
-        self.assertEqual("pallet", self._normal_full_pallet.base_type)
+        self.assertEqual("pallet", self._full_normal_pallet.base_type)
 
     def test_should_adapt_name(self) -> None:
-        self.assertEqual("full", self._normal_full_pallet.name)
+        self.assertEqual("full", self._full_normal_pallet.name)
 
     def test_should_adapt_oversize_values(self) -> None:
         self.fail("DUMMY FAIL")
 
     def test_should_adapt_maximum_dimensions(self) -> None:
-        self.fail("DUMMY FAIL")
+        self.assertEqual(
+            setup.full_normal_max_dims(),
+            self._full_normal_pallet.maximum_dimensions
+        )
+
+        self.assertEqual(
+            setup.double_half_max_dims(),
+            self._double_half_pallet.maximum_dimensions
+        )
 
     def test_should_adapt_maximum_weight(self) -> None:
-        self.assertEqual(1200, self._normal_full_pallet.maximum_weight)
-        self.assertEqual(600, self._double_half_pallet_adaptor.maximum_weight)
+        self.assertEqual(1200, self._full_normal_pallet.maximum_weight)
+        self.assertEqual(600, self._double_half_pallet.maximum_weight)
 
     def test_should_adapt_override_options(self) -> None:
-        self.assertListEqual([], self._normal_full_pallet.override_options)
+        self.assertListEqual([], self._full_normal_pallet.override_options)
 
 
 if __name__ == '__main__':
--- src/test/upn/api/adaptors/network_pallet/setup.py
+++ src/test/upn/api/adaptors/network_pallet/setup.py
@@ -1,9 +1,24 @@
 from src.main.upn.api.data_structures.network_pallet.structure \
     import NetworkPallet
 
+from src.main.graylaw.cargo.metrics.dimensions import (
+    Dimensions, DimensionsInMetres)
+
 
 def full_normal_pallet() -> NetworkPallet:
     return _network_pallet("N", "FULL")
+
+
+def full_normal_max_dims() -> Dimensions:
+    return _dimensions(1.0, 1.2, 2.0)
+
+
+def double_half_max_dims() -> Dimensions:
+    return _dimensions(1.0, 1.2, 2.0)
+
+
+def oversize_values() -> None:
+    return None
 
 
 def double_half_pallet() -> NetworkPallet:
@@ -16,5 +31,14 @@ def _network_pallet(pallet_size: str, pallet_type: str) -> NetworkPallet:
     result.pallet_size = pallet_size
     result.pallet_type = pallet_type
     result.barcode = "W213359800P"
+
+    return result
+
+
+def _dimensions(length: float, width: float, height: float) -> Dimensions:
+    result = DimensionsInMetres()
+    result.length = length
+    result.width = width
+    result.height = height
 
     return result
