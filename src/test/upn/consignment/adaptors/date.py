aca9752a66a9d0cf06d59ada46c1d3dac26906ca
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-22 10:34:30 +0000

    Set up tests for equality operators on the date adaptor.

--- src/test/upn/consignment/adaptors/date.py
+++ src/test/upn/consignment/adaptors/date.py
@@ -1,12 +1,13 @@
 import datetime
 import unittest
 from src.main.upn.consignment.adaptors.date import UPNDateAdaptor
+from src.main.graylaw.shipment_dates.date.model import Date as GraylawDate
 
 
 class TestDateAdaptor(unittest.TestCase):
     def setUp(self):
-        self._date = datetime.datetime(day=3, month=6, year=1991)
-        self._adaptor = UPNDateAdaptor(self._date)
+        date = datetime.datetime(day=3, month=6, year=1991)
+        self._adaptor = UPNDateAdaptor(date)
 
     def test_should_get_day(self) -> None:
         self.assertEqual(3, self._adaptor.day)
@@ -16,6 +17,24 @@ class TestDateAdaptor(unittest.TestCase):
 
     def test_should_get_year(self) -> None:
         self.assertEqual(1991, self._adaptor.year)
+
+    def test_should_show_date_as_equal(self) -> None:
+        graylaw_date = GraylawDate("03/06/1991")
+        self.assertEqual(graylaw_date, self._adaptor)
+        self.assertFalse(graylaw_date < self._adaptor)
+        self.assertFalse(graylaw_date > self._adaptor)
+
+    def test_should_show_date_as_earlier_than(self) -> None:
+        graylaw_date = GraylawDate("04/06/1991")
+        self.assertLess(graylaw_date, self._adaptor)
+        self.assertFalse(graylaw_date == self._adaptor)
+        self.assertFalse(graylaw_date > self._adaptor)
+
+    def test_should_show_date_as_later_than(self) -> None:
+        graylaw_date = GraylawDate("02/06/1991")
+        self.assertGreater(graylaw_date, self._adaptor)
+        self.assertFalse(graylaw_date == self._adaptor)
+        self.assertFalse(graylaw_date < self._adaptor)
 
 
 if __name__ == '__main__':
