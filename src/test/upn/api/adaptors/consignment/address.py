055bd395b74d199f4a439690a36536393ba0d28e
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-22 08:24:53 +0000

    Dummy test for address adaptor.

---
+++ src/test/upn/api/adaptors/consignment/address.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestUPNAddressAdaptor(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_should_adapt_upn_address(self) -> None:
+        self.fail("DUMMY FAIL")
+
+
+if __name__ == '__main__':
+    unittest.main()
