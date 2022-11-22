79c77febd6ca68c1fc54c67ad47f24c27bbd244a
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-22 17:32:53 +0000

    Extracted Cargo Entry model.

--- src/main/graylaw/cargo/entries/implementation.py
+++ src/main/graylaw/cargo/entries/implementation.py
@@ -1,72 +1,37 @@
-from __future__ import annotations
-import copy
 from src.main.graylaw.cargo.packages.types.interface import PackageType
 from src.main.graylaw.cargo.entries import interface
-
-from src.main.graylaw.cargo.entries.validation \
-    import CargoEntryValidationStrategy
+from src.main.graylaw.cargo.entries.model import CargoEntryModel
 
 
 class CargoEntry(interface.CargoEntry):
     def __init__(self, package: PackageType, quantity: int, weight: float):
-        self._package_type = copy.deepcopy(package)
-        self._quantity = quantity
-        self._weight = weight
-        self._validation = CargoEntryValidationStrategy(entry_to_validate=self)
+        self._model = CargoEntryModel(package, quantity, weight)
 
     @property
     def package_type(self) -> PackageType:
-        return self._package_type
+        return self._model.package_type
 
     @property
     def quantity(self) -> int:
-        return self._quantity
+        return self._model.quantity
 
     @quantity.setter
     def quantity(self, new_quantity: int) -> None:
-        if not self._validation.is_quantity_valid(new_quantity):
-            raise ValueError(
-                "Desired number of packages will exceed maximum average "
-                "weight."
-            )
-
-        self._quantity = new_quantity
+        self._model.quantity = new_quantity
 
     @property
     def weight(self) -> float:
-        return self._weight
+        return self._model.weight
 
     @weight.setter
-    def weight(self, weight: float) -> None:
-        if not self._validation.is_total_weight_valid(weight):
-            raise ValueError(
-                "Desired weight of", weight, " will exceed average "
-                "maximum of", self.package_type.maximum_weight, " spread "
-                "across", self._quantity, " packages."
-            )
-
-        self._weight = weight
+    def weight(self, new_weight: float) -> None:
+        self._model.weight = new_weight
 
     def set_totals(self, quantity: int, weight: float) -> None:
-        if not self._validation.are_metrics_valid(quantity, weight):
-            raise ValueError("Totals exceed average maximum weight.")
+        self._model.set_totals(quantity, weight)
 
-        self._quantity = quantity
-        self._weight = weight
+    def __eq__(self, other: interface.CargoEntry) -> bool:
+        return self._model.__eq__(other)
 
-    def __eq__(self, other_entry: CargoEntry) -> bool:
-        return self._package_type == other_entry.package_type
-
-    def __iadd__(self, other: CargoEntry) -> CargoEntry:
-        if not self == other:
-            raise ValueError("Incorrect pallet types to combine.")
-
-        self._append_cargo(other)
-
-        return self
-
-    def _append_cargo(self, other: interface.CargoEntry) -> None:
-        self.set_totals(
-            quantity=self.quantity + other.quantity,
-            weight=self.weight + other.weight
-        )
+    def __iadd__(self, other: interface.CargoEntry) -> interface.CargoEntry:
+        return self._model.__iadd__(other)
