ee806330075c9cd12bc353b9eaa9159732f6b88f
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-22 09:21:16 +0000

    Started test for UPN Dates adaptor.

---
+++ src/test/upn/api/adaptors/consignment/dates.py
@@ -0,0 +1,15 @@
+import unittest
+from src.main.upn.consignments.dates import UPNDates
+from src.main.upn.api.adaptors.dates import UPNDatesAdaptor
+
+
+class TestUPNDatesAdaptor(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_should_adapt_upn_address(self) -> None:
+        self.fail("MOCK FAIL")
+
+
+if __name__ == '__main__':
+    unittest.main()
