bdfd4cf006df365a43e92b7c00f9efd2ea31c3fd
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-22 17:52:05 +0000

    Started filling out cargo adaptor.

--- src/main/upn/consignment/adaptors/cargo.py
+++ src/main/upn/consignment/adaptors/cargo.py
@@ -1,9 +1,32 @@
+from src.main.upn.consignment.structures.cargo import UPNCargo
+from src.main.graylaw.cargo.container.interface import Cargo
+from src.main.graylaw.cargo.entries.interface import CargoEntry
 
 
-class UPNCargoAdaptor:
+class UPNCargoAdaptor(Cargo):
     """Class for adapting a UPN Cargo structure into a Graylaw Cargo
     structure.
     """
-    def __init__(self):
-        pass
+    def __init__(self, upn_cargo: UPNCargo):
+        self._cargo = upn_cargo
 
+    def __contains__(self, entry: CargoEntry) -> bool:
+        ...
+
+    def __iter__(self):
+        ...
+
+    def __getitem__(self, index: int) -> CargoEntry:
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def add(self, entry: CargoEntry) -> None:
+        ...
+
+    def clear(self) -> None:
+        ...
+
+    def index_by_package_type(self, package_type: PackageType) -> CargoEntry:
+        ...
