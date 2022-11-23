6d97b6fef637195a86d90ebb6a27b232e81a0a68
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-23 13:15:31 +0000

    Passes test for getting cargo entry.

--- src/test/graylaw/cargo/container/model.py
+++ src/test/graylaw/cargo/container/model.py
@@ -9,6 +9,17 @@ class TestCargoModel(unittest.TestCase):
     def test_should_total_weight(self) -> None:
         self.assertEqual(4200, self._cargo.total_weight)
 
+    def test_should_get_cargo_entries(self) -> None:
+        entry_1 = self._cargo[0]
+        self.assertEqual(3000, entry_1.weight)
+        self.assertEqual(3, entry_1.quantity)
+        self.assertEqual("full", entry_1.package_type.name)
+
+        entry_2 = self._cargo[1]
+        self.assertEqual(1200, entry_2.weight)
+        self.assertEqual(2, entry_2.quantity)
+        self.assertEqual("half", entry_2.package_type.name)
+
 
 if __name__ == "__main__":
     unittest.main()
