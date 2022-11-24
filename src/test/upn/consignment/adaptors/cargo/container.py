ba6620decfd27bd9e870c776f6fb32a4de1eaa57
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-24 11:05:51 +0000

    Failing test added for len of cargo entries.

--- src/test/upn/consignment/adaptors/cargo/container.py
+++ src/test/upn/consignment/adaptors/cargo/container.py
@@ -10,6 +10,9 @@ class TestUPNCargoAdaptor(unittest.TestCase):
     def test_should_adapt_total_weight(self) -> None:
         self.assertEqual(3000, self._adaptor.total_weight)
 
+    def test_should_return_number_of_entries(self) -> None:
+        self.assertEqual(1, len(self._adaptor))
+
 
 if __name__ == '__main__':
     unittest.main()
