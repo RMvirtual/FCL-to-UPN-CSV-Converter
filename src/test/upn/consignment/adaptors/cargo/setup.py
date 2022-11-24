4665d2a62a46f1ca96127983322ac06af7c1cb44
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-24 10:32:38 +0000

    Builder class for UPN Pallet implemented into the upn cargo container adaptor test.

--- src/test/upn/consignment/adaptors/cargo/setup.py
+++ src/test/upn/consignment/adaptors/cargo/setup.py
@@ -1,22 +1,22 @@
 from src.main.upn.consignment.structures.cargo.container.container \
     import UPNCargo
 
-from src.main.upn.api.data_structures.network_pallet.implementation \
-    import NetworkPallet
+from src.main.upn.consignment.structures.cargo.package.builder \
+    import UPNPalletBuilder
 
 
 def single_entry_upn_cargo() -> UPNCargo:
     result = UPNCargo()
     result.total_weight = 3000
-    network_pallet = NetworkPallet()
-    network_pallet.consignment_barcode = "W123456789"
-    network_pallet.barcode = "W987654321"
-    network_pallet.size = "N"
-    network_pallet.type = "FULL"
+    builder = UPNPalletBuilder()
+    builder.set_size_constraints(["N"])
+    builder.set_type_constraints(["FULL"])
+    builder.set_barcode("W123456789")
+    builder.set_consignment_barcode("W987654321")
+    builder.set_size("N")
+    builder.set_type("FULL")
 
     for i in range(3):
-        result.pallets.append(network_pallet)
+        result.pallets.append(builder.build())
 
     return result
-
-
