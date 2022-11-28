7c584979bbe6f9e00fe408e9b60c6bd1ede2a247
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-28 17:08:46 +0000

    Minor name clarification in file system calls for Graylaw.

--- src/main/file_system/companies/graylaw/cargo_types.py
+++ src/main/file_system/companies/graylaw/cargo_types.py
@@ -1,16 +1,16 @@
 from src.main.file_system.file_readers import runfiles
 
 
-def _freight_resource(relative_path: str) -> str:
+def _graylaw_resource(relative_path: str) -> str:
     return "resources/companies/graylaw/" + relative_path
 
 
 def base_packages_file():
     return runfiles.load_json_file(
-        _freight_resource("cargo_types/base_packages.json"))
+        _graylaw_resource("cargo_types/base_packages.json"))
 
 
 def oversize_options():
     return runfiles.load_json_file(
-        _freight_resource("cargo_types/oversize_options.json"))
+        _graylaw_resource("cargo_types/oversize_options.json"))
 
--- src/main/file_system/companies/graylaw/oversize_options.py
+++ src/main/file_system/companies/graylaw/oversize_options.py
@@ -1,10 +1,10 @@
 from src.main.file_system.file_readers import runfiles
 
 
-def _freight_resource(relative_path: str) -> str:
+def _graylaw_resource(relative_path: str) -> str:
     return "resources/companies/graylaw/" + relative_path
 
 
 def contents() -> list[dict[str, str]]:
     return runfiles.load_json_file(
-        _freight_resource("cargo_types/oversize_options.json"))
+        _graylaw_resource("cargo_types/oversize_options.json"))
