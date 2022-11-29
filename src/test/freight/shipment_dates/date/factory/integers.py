3a92bad12bfa057ce67a1b3f0f08a72d78730f88
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-29 12:25:14 +0000

    Failing test for dates as integers factory.

--- src/test/freight/shipment_dates/date/factory/integers.py
+++ src/test/freight/shipment_dates/date/factory/integers.py
@@ -1,9 +1,19 @@
 import unittest
+from src.main.freight.shipment_dates.date.factory.integers \
+    import DateAsIntegersFactory
 
 
 class TestDatesAsIntegersFactory(unittest.TestCase):
     def setUp(self):
-        pass
+        self._factory = DateAsIntegersFactory()
 
-    def test_should_fail(self):
-        self.fail()
+    def test_should_create_from_dd_mm_yyyy(self):
+        date = self._factory.from_dd_mm_yy("03061991")
+
+        self.assertEqual(3, date.day)
+        self.assertEqual(6, date.month)
+        self.assertEqual(1991, date.year)
+
+
+if __name__ == '__main__':
+    unittest.main()
