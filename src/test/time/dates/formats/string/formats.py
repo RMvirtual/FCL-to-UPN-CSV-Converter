ec8ec53d839570ac4b5051bf8307d7cc630f1abc
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-29 16:23:49 +0000

    Whitespace formatter works.

--- src/test/time/dates/formats/string/formats.py
+++ src/test/time/dates/formats/string/formats.py
@@ -3,33 +3,40 @@ from src.main.time.dates.formats.string import formats
 
 
 class TestDateFormatRecognition(unittest.TestCase):
-    def test_should_create_dd_mm_yy_format(self) -> None:
+    def test_should_format_dd_mm_yy(self) -> None:
         result = formats.DDMMYY("030691")
 
         self.assertEqual(3, result.day)
         self.assertEqual(6, result.month)
         self.assertEqual(91, result.year)
 
-    def test_should_create_dd_mm_yyyy_format(self) -> None:
+    def test_should_format_dd_mm_yyyy(self) -> None:
         result = formats.DDMMYYYY("03061991")
 
         self.assertEqual(3, result.day)
         self.assertEqual(6, result.month)
         self.assertEqual(1991, result.year)
 
-    def test_should_create_dd_mm_yy_with_separators_format(self) -> None:
+    def test_should_format_dd_mm_yy_with_separators(self) -> None:
         for date in ["03/06/91", "03.06.91"]:
             result = formats.NumericWithSeparators(date)
             self.assertEqual(3, result.day)
             self.assertEqual(6, result.month)
             self.assertEqual(91, result.year)
 
-    def test_should_create_dd_mm_yyyy_with_separators_format(self) -> None:
+    def test_should_format_dd_mm_yyyy_with_separators(self) -> None:
         for date in ["03/06/1991", "03.06.1991"]:
             result = formats.NumericWithSeparators(date)
             self.assertEqual(3, result.day)
             self.assertEqual(6, result.month)
             self.assertEqual(1991, result.year)
+
+    def test_should_format_dd_mm_yyyy_with_whitespace(self) -> None:
+        result = formats.NumericWithSeparators("03 06 91")
+
+        self.assertEqual(3, result.day)
+        self.assertEqual(6, result.month)
+        self.assertEqual(91, result.year)
 
 
 if __name__ == '__main__':
