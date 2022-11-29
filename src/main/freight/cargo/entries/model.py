dc1a8bb28a26bec78f141821290a3736741c9ce2
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-29 10:18:43 +0000

    Moved validation assertions over to the validation class.

--- src/main/freight/cargo/entries/model.py
+++ src/main/freight/cargo/entries/model.py
@@ -1,6 +1,5 @@
 import copy
 from src.main.freight.cargo.entries.interface import CargoEntry
-from src.main.freight.cargo.entries import exceptions
 from src.main.freight.cargo.entries.validation import EntryValidationStrategy
 from src.main.freight.cargo.packages.types.interface import PackageType
 
@@ -10,7 +9,7 @@ class CargoEntryModel(CargoEntry):
         self._package_type = copy.deepcopy(package)
         self._quantity = quantity
         self._weight = weight
-        self._validation = EntryValidationStrategy(entry_to_validate=self)
+        self._validation = EntryValidationStrategy(self)
 
     @property
     def package_type(self) -> PackageType:
@@ -22,7 +21,7 @@ class CargoEntryModel(CargoEntry):
 
     @quantity.setter
     def quantity(self, new_quantity: int) -> None:
-        self._assert_quantity_is_valid(new_quantity)
+        self._validation.assert_quantity_is_valid(new_quantity)
         self._quantity = new_quantity
 
     @property
@@ -31,11 +30,11 @@ class CargoEntryModel(CargoEntry):
 
     @weight.setter
     def weight(self, new_weight: float) -> None:
-        self._assert_total_weight_is_valid(new_weight)
+        self._validation.assert_total_weight_is_valid(new_weight)
         self._weight = new_weight
 
     def set_totals(self, quantity: int, weight: float) -> None:
-        self._assert_metrics_are_valid(quantity, weight)
+        self._validation.assert_metrics_are_valid(quantity, weight)
         self._quantity = quantity
         self._weight = weight
 
@@ -43,33 +42,14 @@ class CargoEntryModel(CargoEntry):
         return self._package_type == other_entry.package_type
 
     def __iadd__(self, other: CargoEntry) -> CargoEntry:
-        self._assert_cargo_entry_matches(other)
         self._append_cargo(other)
 
         return self
 
     def _append_cargo(self, other: CargoEntry) -> None:
+        self._validation.assert_cargo_entry_is_compatible(other)
+
         self.set_totals(
             quantity=self.quantity + other.quantity,
             weight=self.weight + other.weight
         )
-
-    def _assert_metrics_are_valid(self, quantity: int, weight: float) -> None:
-        if not self._validation.are_metrics_valid(quantity, weight):
-            raise exceptions.TotalsInvalid()
-
-    def _assert_quantity_is_valid(self, new_quantity: int) -> None:
-        if not self._validation.is_quantity_valid(new_quantity):
-            raise exceptions.QuantityInvalid()
-
-    def _assert_total_weight_is_valid(self, new_weight: float) -> None:
-        if not self._validation.is_total_weight_valid(new_weight):
-            raise exceptions.TotalWeightInvalid(
-                total_weight=new_weight,
-                max_weight_per_package=self.package_type.maximum_weight,
-                total_quantity=self.quantity
-            )
-
-    def _assert_cargo_entry_matches(self, other: CargoEntry) -> None:
-        if not self == other:
-            raise exceptions.PackageTypesInvalid()
--- src/main/freight/cargo/entries/validation.py
+++ src/main/freight/cargo/entries/validation.py
@@ -1,4 +1,5 @@
 from src.main.freight.cargo.entries.interface import CargoEntry
+from src.main.freight.cargo.entries import exceptions
 
 
 class EntryValidationStrategy:
@@ -22,3 +23,23 @@ class EntryValidationStrategy:
     @staticmethod
     def _average_weight(quantity: int, weight: float) -> float:
         return weight / quantity if quantity else 0
+
+    def assert_metrics_are_valid(self, quantity: int, weight: float) -> None:
+        if not self.are_metrics_valid(quantity, weight):
+            raise exceptions.TotalsInvalid()
+
+    def assert_quantity_is_valid(self, new_quantity: int) -> None:
+        if not self.is_quantity_valid(new_quantity):
+            raise exceptions.QuantityInvalid()
+
+    def assert_total_weight_is_valid(self, new_weight: float) -> None:
+        if not self.is_total_weight_valid(new_weight):
+            raise exceptions.TotalWeightInvalid(
+                total_weight=new_weight,
+                max_weight_per_package=self._entry.package_type.maximum_weight,
+                total_quantity=self._entry.quantity
+            )
+
+    def assert_cargo_entry_is_compatible(self, other: CargoEntry) -> None:
+        if not self._entry == other:
+            raise exceptions.PackageTypesInvalid()
