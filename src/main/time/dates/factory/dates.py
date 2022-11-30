7ee98ca6b71b615aacbc08e8634b2348cad01769
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-30 15:00:36 +0000

    Partial upload of work so far. Removed pointless dataclasses for dates now DateFormat does most of the work.

--- src/main/time/dates/factory/dates.py
+++ src/main/time/dates/factory/dates.py
@@ -1,29 +1,12 @@
-from src.main.time.dates.formats.recognition import DateFormat
-from src.main.time.dates.formats.implementation import (
-    AlphanumericFormatter, NumericDelimitedFormatter, DDMMYY)
-
+from src.main.time.dates.formats import factory
 from src.main.time.dates.implementation.date import Date
 from src.main.time.dates.interface.date import DateInterface
 
 
 def from_string(date: str) -> DateInterface:
-    date_format = DateFormat(date)
+    date_formatter = factory.formatter(date)
 
-    if date_format.is_unrecognised():
-        raise ValueError("Cannot parse date with format of " + date)
-
-    formatter = None
-
-    if date_format.is_numeric():
-        formatter = DDMMYY(date)
-
-    elif date_format.is_numeric_delimited():
-        formatter = NumericDelimitedFormatter(date)
-
-    elif date_format.is_alphanumeric():
-        formatter = AlphanumericFormatter(date)
-
-    return Date(formatter.day, formatter.month, formatter.year)
+    return Date(date_formatter.day, date_formatter.month, date_formatter.year)
 
 
 def from_integers(day: int, month: int, year: int):
--- src/test/time/dates/factory/factory.py
+++ src/test/time/dates/factory/factory.py
@@ -1,4 +1,5 @@
 import unittest
+from src.main.time.dates import factory
 
 
 class TestDateFactory(unittest.TestCase):
@@ -7,32 +8,23 @@ class TestDateFactory(unittest.TestCase):
         self._correct_month = 10
         self._correct_year = 2022
 
-    def test_should_create_from_ddmmyyyy_without_separators(self) -> None:
+    def test_should_create_from_numeric_date(self) -> None:
+        dates = ["231022", "23102022"]
         self.fail()
 
-    def test_should_create_from_ddmmyyyy_with_separators(self) -> None:
-        dates = ("23\\10\\2022", "23-10-2022", "23/10/2022", "23.10.2022")
+    def test_should_create_from_numeric_delimited_date(self) -> None:
+        dates = [
+            "23\\10\\2022", "23-10-2022", "23/10/2022", "23.10.2022",
+            "23\\10\\22", "23-10-22", "23/10/22", "23.10.22"
+        ]
+
         self.fail()
 
-    def test_should_create_from_ddmmyy_without_separators(self) -> None:
-        date = "231022"
-        self.fail()
-
-    def test_should_create_from_ddmmyy_with_separators(self) -> None:
-        dates = ("23\\10\\22", "23-10-22", "23/10/22", "23.10.22")
-        self.fail()
-
-    def test_should_create_from_dd_mmm_yyyy_(self) -> None:
-        dates = ("23-Oct-2022", "23-Oct-22")
-        self.fail()
-
-    def test_should_create_from_dd_mmmmm_yyyy_(self) -> None:
-        dates = ("23-April-2022", "23-April-22")
-        self.fail()
-
-    def test_should_create_from_date_with_full_month_name(self) -> None:
-        dates = ("23-September-2022", "23/September/22", " 23 September 2022")
-        self.fail()
+    def test_should_create_from_alphanumeric_date(self) -> None:
+        dates = [
+            "23-Oct-2022", "23-Oct-22", "23-April-2022", "23-April-22",
+            "23-September-2022", "23/September/22", " 23 September 2022"
+        ]
 
 
 if __name__ == '__main__':
