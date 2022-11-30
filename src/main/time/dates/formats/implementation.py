db3219aa7ac59373d1853c7ae8a682693ca83cd4
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-11-30 14:27:23 +0000

    Formats passing tests.

--- src/main/time/dates/formats/implementation.py
+++ src/main/time/dates/formats/implementation.py
@@ -8,59 +8,74 @@ from src.main.time.years import factory as years_factory
 class NumericFormatter(DateFormatter):
     def __init__(self, date: str):
         validation.assert_numeric_format_is_valid(date)
-        self._date = date
+        self._day = int(date[0:2])
+        self._month = int(date[2:4])
+        self._year = years_factory.full_year(date[4:])
 
     @property
     def day(self) -> int:
-        return int(self._date[0:2])
+        return self._day
 
     @property
     def month(self) -> int:
-        return int(self._date[2:4])
+        return self._month
 
     @property
     def year(self) -> int:
-        return years_factory.full_year(self._date[4:])
+        return self._year
 
 
 class NumericDelimitedFormatter(DateFormatter):
     def __init__(self, date: str):
-        split_parts = re.split(r"\W+", date)
-        self._parts = list(map(
-            int, filter(lambda date_part: bool(date_part), split_parts)))
+        date_items = self._itemise_date(date)
+
+        self._day = date_items[0]
+        self._month = date_items[1]
+        self._year = years_factory.full_year(date_items[2])
+
+    def _itemise_date(self, date: str) -> list[int]:
+        return list(map(int, self._split_string_by_non_word_values(date)))
+
+    @staticmethod
+    def _split_string_by_non_word_values(string: str) -> filter:
+        return filter(lambda part: bool(part), re.split(r"\W+", string))
 
     @property
     def day(self) -> int:
-        return self._parts[0]
+        return self._day
 
     @property
     def month(self) -> int:
-        return self._parts[1]
+        return self._month
 
     @property
     def year(self) -> int:
-        return years_factory.full_year(self._parts[2])
+        return self._year
 
 
 class AlphanumericFormatter(DateFormatter):
     def __init__(self, date: str):
-        split_parts = re.split(r"\W+", date)
-        cleaned_parts = list(filter(lambda d: bool(d), split_parts))
+        date_items = self._itemise_date(date)
 
-        self._parts = [
-            int(cleaned_parts[0]),
-            months_factory.month_no(cleaned_parts[1]),
-            years_factory.full_year(cleaned_parts[2])
-        ]
+        self._day = int(date_items[0])
+        self._month = months_factory.month_no(date_items[1])
+        self._year = years_factory.full_year(date_items[2])
+
+    def _itemise_date(self, date: str) -> list[str]:
+        return list(self._split_string_by_non_words(date))
+
+    @staticmethod
+    def _split_string_by_non_words(string: str) -> filter:
+        return filter(lambda part: bool(part), re.split(r"\W+", string))
 
     @property
     def day(self) -> int:
-        return self._parts[0]
+        return self._day
 
     @property
     def month(self) -> int:
-        return self._parts[1]
+        return self._month
 
     @property
     def year(self) -> int:
-        return self._parts[2]
+        return self._year
