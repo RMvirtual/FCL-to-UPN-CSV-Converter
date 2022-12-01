9bdb1483f9ff5827d55923de5eedf9a434fa22b9
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-01 15:31:10 +0000

    Basic premium service tests passing.

--- src/test/freight/service/premium_service.py
+++ src/test/freight/service/premium_service.py
@@ -6,7 +6,7 @@ class TestPremiumService(unittest.TestCase):
     def setUp(self):
         self._service = PremiumService()
 
-    def test_should_set_service_as_am(self) -> None:
+    def test_should_change_service(self) -> None:
         self._service.am()
         self.assertTrue(self._service.is_am())
         self.assertFalse(self._service.is_timed())
@@ -15,6 +15,13 @@ class TestPremiumService(unittest.TestCase):
         self.assertFalse(self._service)
         self._service.pre_10am()
         self.assertTrue(self._service)
+
+    def test_should_show_two_services_as_equal(self) -> None:
+        other_service = PremiumService()
+        self.assertEqual(self._service, other_service)
+
+        other_service.am()
+        self.assertNotEqual(self._service, other_service)
 
 
 if __name__ == "__main__":
