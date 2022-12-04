d99c7abd7e199abd39e56dd12e7f595a636ef278
RMvirtual <rmvirtual@AOL.com>
2022-12-04 12:09:33 +0000

    Started UPN database library.

---
+++ src/test/companies/upn/database/pallets.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestUPNPalletDatabase(unittest.TestCase):
+    def setUp(self):
+        ...
+
+    def test_should_add_pallet(self) -> None:
+        self.fail("MOCK FAIL")
+
+
+if __name__ == '__main__':
+    unittest.main()
