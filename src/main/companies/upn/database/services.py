089522c43ce6298cdce62f24882a7ef612ac8ace
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-06 11:35:54 +0000

    Cleaned up database tests.

--- src/main/companies/upn/database/services.py
+++ src/main/companies/upn/database/services.py
@@ -3,10 +3,25 @@ from src.main.companies.upn.interfaces.services.service import UPNService
 from src.main.companies.upn.implementations.services.specific.service \
     import UPNService as UPNServiceImpl
 
+from src.main.companies.upn.interfaces.services.container \
+    import UPNServices as UPNServicesProvider
+
+from src.main.companies.upn.implementations.services.container.container \
+    import ServicesDetails, UPNServices
+
 
 class UPNServicesDatabase:
     def __init__(self):
         self._data_file = api.network_consignment()
+
+    def all_services(self) -> UPNServicesProvider:
+        fields = ServicesDetails()
+        fields.main = self.main_service()
+        fields.premium = self.premium_service()
+        fields.tail_lift = self.tail_lift_required()
+        fields.additional = self.additional_service()
+
+        return UPNServices(fields)
 
     def main_service(self) -> UPNService:
         return self._service_from_data_file("main_service")
