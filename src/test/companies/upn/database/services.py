7151fb88982e8a64542faa67d7a9724b0c490cd8
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-06 11:18:54 +0000

    Database getters passes.

--- src/test/companies/upn/database/services.py
+++ src/test/companies/upn/database/services.py
@@ -12,6 +12,27 @@ class TestUPNServicesDatabase(unittest.TestCase):
         self.assertListEqual(["P", "S", "I", "R"], result.constraints())
         self.assertEqual("P", result.selection)
 
+    def test_should_get_premium_service_for_network_consignment(self) -> None:
+        result = self._database.premium_service()
+
+        self.assertListEqual(
+            ["", "B10", "AM", "Timed", "Sat"], result.constraints())
+        self.assertEqual("", result.selection)
+
+    def test_should_get_tail_lift_service_for_network_consignment(
+            self) -> None:
+        result = self._database.tail_lift_required()
+
+        self.assertListEqual(["", "TLift"], result.constraints())
+        self.assertEqual("", result.selection)
+
+    def test_should_get_additional_service_for_network_consignment(
+            self) -> None:
+        result = self._database.additional_service()
+
+        self.assertListEqual(["", "BI", "Bkd", "OOH"], result.constraints())
+        self.assertEqual("", result.selection)
+
 
 if __name__ == '__main__':
     unittest.main()
