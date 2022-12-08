8d3e47bb9e02c05dfb4da509fa6bb8005ffcc2b6
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-08 10:20:46 +0000

    Pallet database passing again.

--- src/main/companies/upn/database/pallets.py
+++ src/main/companies/upn/database/pallets.py
@@ -1,13 +1,15 @@
-from src.main.file_system.companies.upn import api
-from src.main.companies.upn.interfaces.api.pallets \
-    import CustPallet, NetworkPallet
 from src.main.companies.upn.implementations.pallets.network import factory
+from src.main.companies.upn.interfaces.api.pallets import (
+    CustPallet, NetworkPallet)
+
+from src.main.file_system.companies.upn.api.pallets.network \
+    import NetworkPalletFiles
 
 
 class UPNPalletsDatabase:
     """Factory for loading pallet types from UPN system configuration."""
     def __init__(self):
-        self._data_file = api.network_pallet_constraints()
+        self._data_file = NetworkPalletFiles()
 
     def network_pallet(self) -> NetworkPallet:
         return self._deserialise_network_pallet()
@@ -16,8 +18,11 @@ class UPNPalletsDatabase:
         ...
 
     def _deserialise_network_pallet(self) -> NetworkPallet:
+        constraints = self._data_file.constraints
+        keys = self._data_file.keys
+
         return factory.network_pallet(
             type_name="FULL", size_name="N",
-            type_constraints=self._data_file["type"]["values"],
-            size_constraints=self._data_file["size"]["values"],
+            type_constraints=constraints[keys["type"]]["values"],
+            size_constraints=constraints[keys["size"]]["values"],
         )
