bdd52083e2510b4dd0f35070aac95ff36eaafc4f
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-08 10:33:44 +0000

    Passes network consignment files loading.

--- src/test/file_system/companies/upn/api/consignments.py
+++ src/test/file_system/companies/upn/api/consignments.py
@@ -7,13 +7,13 @@ class TestUPNConsignmentFiles(unittest.TestCase):
     def setUp(self) -> None:
         self._files = NetworkConsignmentFiles()
 
-    def test_can_load_upn_network_pallet_keys(self) -> None:
-        self.assertEqual("ConBarcode", self._files.keys["consignment_barcode"])
+    def test_can_load_upn_network_consignment_keys(self) -> None:
+        self.assertEqual("ConBarcode", self._files.keys["barcode"])
 
-    def test_can_load_upn_network_pallet_constraints(self) -> None:
+    def test_can_load_upn_network_consignment_constraints(self) -> None:
         self.assertDictEqual(
-            {"type": "string", "values": ["N", "O", "2", "3"]},
-            self._files.constraints["PalletSize"]
+            {"type": "string", "values": ["P", "S", "I", "R"]},
+            self._files.constraints["MainService"]
         )
 
 
