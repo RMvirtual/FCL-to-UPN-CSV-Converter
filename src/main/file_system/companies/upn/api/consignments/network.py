6d8cabb4e4543869b232b01225019fd4c93960b1
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-08 10:31:45 +0000

    Set up test for network consignment file reader.

--- src/main/file_system/companies/upn/api/consignments/network.py
+++ src/main/file_system/companies/upn/api/consignments/network.py
@@ -3,15 +3,15 @@ from src.main.file_system.companies.upn.api.keys.keys_with_constraints \
     import KeysWithConstraints
 
 
-class NetworkPalletFiles(KeysWithConstraints):
+class NetworkConsignmentFiles(KeysWithConstraints):
     def __init__(self):
         super().__init__(self._keys_path(), self._constraints_path())
 
     def _keys_path(self) -> str:
-        return self._absolute_path("pallets/network/keys.json")
+        return self._absolute_path("consignments/network/keys.json")
 
     def _constraints_path(self):
-        return self._absolute_path("pallets/network/constraints.json")
+        return self._absolute_path("consignments/network/constraints.json")
 
     def _absolute_path(self, file_name: str):
         return runfiles.absolute_path(self._api_file_path(file_name))
