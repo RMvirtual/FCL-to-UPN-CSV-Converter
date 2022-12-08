1744db586c08af7f39ae9abf134b0fa72f2bbe80
RMvirtual <61029123+RMvirtual@users.noreply.github.com>
2022-12-08 16:26:36 +0000

    Refactored item constraints test.

--- src/test/companies/upn/api/constraints/implementation.py
+++ src/test/companies/upn/api/constraints/implementation.py
@@ -4,13 +4,14 @@ from src.main.companies.upn.api.constraints.implementation \
 
 
 class TestItemConstraint(unittest.TestCase):
+    def setUp(self):
+        self._constraint = ItemConstraint(str, ["Hello", "World"])
+
     def test_should_get_type_from_item_constraint(self) -> None:
-        result = ItemConstraint(str, ["Hello", "World"])
-        self.assertEqual(str, result.type)
+        self.assertEqual(str, self._constraint.type)
 
     def test_should_get_values_from_constraint(self) -> None:
-        result = ItemConstraint(str, ["Hello", "World"])
-        self.assertListEqual(["Hello", "World"], result.values)
+        self.assertListEqual(["Hello", "World"], self._constraint.values)
 
     def test_should_raise_exception_with_heterogenous_values(self) -> None:
         with self.assertRaises(TypeError):
